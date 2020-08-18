1c77b5ca366a47ee27f657c332da58f2905faad1
Rounak Nayak <44160407+rounakmindtree@users.noreply.github.com>
2020-08-18 12:24:44 +0530

    MOSIP-8960 Update the design and db documentation for ID Repo

--- db_release_scripts/mosip_release_commons_db_deployment.sh
+++ db_release_scripts/mosip_release_commons_db_deployment.sh
@@ -1,7 +1,7 @@
 ### -- ---------------------------------------------------------------------------------------------------------
 ### -- Script Name		: Release DB deployment
-### -- Deploy Module 	: MOSIP Commons
-### -- Purpose    		: To deploy MOSIP Database alter scripts for the release, Scripts deploy changes for commons databases.       
+### -- Deploy Module 	: MOSIP ID Repository
+### -- Purpose    		: To deploy MOSIP Database alter scripts for the release, Scripts deploy changes for id repository databases.       
 ### -- Create By   		: Sadanandegowda
 ### -- Created Date		: 07-Jan-2020
 ### -- 
@@ -15,14 +15,6 @@ echo "`date` : You logged on to DB deplyment server as : `whoami`"
 echo "`date` : MOSIP Database objects deployment for the release started.... Release Number : $1"
 
 echo "=============================================================================================================="
-bash ./mosip_master/master_release_db_deploy.sh ./mosip_master/master_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "=============================================================================================================="
-bash ./mosip_kernel/kernel_release_db_deploy.sh ./mosip_kernel/kernel_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "=============================================================================================================="
 bash ./mosip_idrepo/idrepo_release_db_deploy.sh ./mosip_idrepo/idrepo_release_deploy.properties $1
 echo "=============================================================================================================="
 
@@ -30,14 +22,5 @@ echo "==========================================================================
 bash ./mosip_idmap/idmap_release_db_deploy.sh ./mosip_idmap/idmap_release_deploy.properties $1
 echo "=============================================================================================================="
 
-echo "=============================================================================================================="
-bash ./mosip_audit/audit_release_db_deploy.sh ./mosip_audit/audit_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "=============================================================================================================="
-bash ./mosip_iam/iam_release_db_deploy.sh ./mosip_iam/iam_release_deploy.properties $1
-echo "=============================================================================================================="
-
-
-echo "`date` : MOSIP DB Release Deployment for core databases is completed, Please check the logs at respective logs directory for more information"
+echo "`date` : MOSIP DB Release Deployment for id repository databases is completed, Please check the logs at respective logs directory for more information"
  
--- db_release_scripts/mosip_revoke_commons_db_deployment.sh
+++ db_release_scripts/mosip_revoke_commons_db_deployment.sh
@@ -1,7 +1,7 @@
 ### -- ---------------------------------------------------------------------------------------------------------
 ### -- Script Name		: Revoke DB deploy
-### -- Deploy Module 	: MOSIP Core
-### -- Purpose    		: To revoke MOSIP Database alter scripts for the release, Scripts revoke changes for core databases.      
+### -- Deploy Module 	: MOSIP ID Repository
+### -- Purpose    		: To revoke MOSIP Database alter scripts for the release, Scripts revoke changes for id repository databases.      
 ### -- Create By   		: Sadanandegowda
 ### -- Created Date		: 07-Jan-2020
 ### -- 
@@ -15,14 +15,6 @@ echo "`date` : You logged on to DB deplyment server as : `whoami`"
 echo "`date` : MOSIP Database objects release deployment revoke started.... Release Number : $1"
 
 echo "=============================================================================================================="
-bash ./mosip_master/master_revoke_db_deploy.sh ./mosip_master/master_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "=============================================================================================================="
-bash ./mosip_kernel/kernel_revoke_db_deploy.sh ./mosip_kernel/kernel_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "=============================================================================================================="
 bash ./mosip_idrepo/idrepo_revoke_db_deploy.sh ./mosip_idrepo/idrepo_release_deploy.properties $1
 echo "=============================================================================================================="
 
@@ -30,13 +22,5 @@ echo "==========================================================================
 bash ./mosip_idmap/idmap_revoke_db_deploy.sh ./mosip_idmap/idmap_release_deploy.properties $1
 echo "=============================================================================================================="
 
-echo "=============================================================================================================="
-bash ./mosip_audit/audit_revoke_db_deploy.sh ./mosip_audit/audit_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "=============================================================================================================="
-bash ./mosip_iam/iam_revoke_db_deploy.sh ./mosip_iam/iam_release_deploy.properties $1
-echo "=============================================================================================================="
-
-echo "`date` : MOSIP DB Release Deployment revoke for core databases is completed, Please check the logs at respective logs directory for more information"
+echo "`date` : MOSIP DB Release Deployment revoke for id repository databases is completed, Please check the logs at respective logs directory for more information"
  
