24dd243f1d102e9832de78877d936263f1d100ac
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2020-09-01 17:42:11 +0530

    Build fix (#21)
    * Injecting publisher client impl
    
    * Revert "Injecting publisher client impl"
    
    This reverts commit 7febe06f49b51d2ed0c8ec6d290c58a4a000c524.
    
    * Revert "Revert "Mosip 8759 id repo notify cred req gen 1 (#18)""
    
    This reverts commit a717ffafb61231a2c6d73d3af65279e2b0c66e1f.
    
    * Added kernel core latest version

---
+++ id-repository/bioextractor-service/src/main/java/io/mosip/bioextractor/constant/BiometricExtractionErrorConstants.java
@@ -0,0 +1,31 @@
+package io.mosip.bioextractor.constant;
+
+public enum BiometricExtractionErrorConstants {
+	
+	UNKNOWN_ERROR("BIE-UNK-001", "Unknown Error"),
+	MISSING_INPUT_PARAMETER("BIE-DAV-001", "Missing Input Parameter - %s"),
+	INVALID_INPUT_PARAMETER("BIE-DAV-002", "Invalid Input Parameter - %s"),
+	INVALID_CBEFF("BIE-DAV-001", "Invalid CBEFF"),
+	TECHNICAL_ERROR("BIE-SDK-001", "Technical Error in Biometric Extraction"),
+
+	;
+	
+	private final String errorCode;
+	private final String errorMessage;
+
+	private BiometricExtractionErrorConstants(String errorCode, String errorMessage) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	
+	
+
+}
