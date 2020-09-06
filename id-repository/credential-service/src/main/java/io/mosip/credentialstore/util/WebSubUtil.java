312849dcc761d28ab67d96e734cd2d188cae9cc9
Sowmya Ujjappa Banakar <43202851+sowmya695@users.noreply.github.com>
2020-09-06 16:37:02 +0530

    MOSIP-8547 code fixed (#45)
    * MOSIP-8547 updated code after testing
    
    * MOSIP-8547 code fixed after testing
    
    Co-authored-by: M1022006 <M1022006@A2ML18485.mindtree.com>

--- id-repository/credential-service/src/main/java/io/mosip/credentialstore/util/WebSubUtil.java
+++ id-repository/credential-service/src/main/java/io/mosip/credentialstore/util/WebSubUtil.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import io.mosip.credentialstore.constants.CredentialConstants;
 
 import io.mosip.idrepository.core.dto.EventModel;
+import io.mosip.idrepository.core.logger.IdRepoLogger;
+import io.mosip.idrepository.core.security.IdRepoSecurityManager;
+import io.mosip.kernel.core.exception.ExceptionUtils;
+import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.websub.spi.PublisherClient;
 import io.mosip.kernel.websub.api.exception.WebSubClientException;
 
@@ -26,16 +30,31 @@ public class WebSubUtil {
 
 	@Autowired
 	RestUtil restUtil;
+
+	private static final Logger LOGGER = IdRepoLogger.getLogger(WebSubUtil.class);
+
+
+	private static final String WEBSUBUTIL = "WebSubUtil";
+
+
+	private static final String REGISTERTOPIC = "registerTopic";
 	
 	public void publishSuccess(String issuer,EventModel eventModel) throws WebSubClientException, IOException{
-
-
-	   pb.registerTopic(issuer+"/"+CredentialConstants.CREDENTIAL_ISSUED, partnerhuburl);
-	
-		HttpHeaders httpHeaders=new HttpHeaders();
+        registerTopic(issuer);
+        HttpHeaders httpHeaders=new HttpHeaders();
 		httpHeaders.add("Cookie",restUtil.getToken());
 		pb.publishUpdate(issuer+"/"+CredentialConstants.CREDENTIAL_ISSUED, eventModel, MediaType.APPLICATION_JSON_UTF8_VALUE, httpHeaders,  partnerhuburl); 
-	
+
+
+	}
+
+	private void registerTopic(String issuer) {
+		try {
+			pb.registerTopic(issuer+"/"+CredentialConstants.CREDENTIAL_ISSUED, partnerhuburl);
+		}catch(WebSubClientException e){
+			LOGGER.error(IdRepoSecurityManager.getUser(), WEBSUBUTIL, REGISTERTOPIC,
+					"Topic already registered");
+		}
 		
 	}
 
