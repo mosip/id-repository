710a82ee7c295f30c568c2711e2cc703cb36ef8d
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-09-16 15:01:12 +0530

    MOSIP-8547 Code review comments fixed

---
+++ id-repository/credential-service/src/main/java/io/mosip/credentialstore/constants/JsonConstants.java
@@ -0,0 +1,28 @@
+package io.mosip.credentialstore.constants;
+
+public class JsonConstants {
+
+	public static final String ID = "id";
+
+	public static final String TYPE = "type";
+
+	public static final String ISSUER = "issuer";
+
+	public static final String ISSUANCEDATE = "issuanceDate";
+
+	public static final String ISSUEDTO = "issuedTo";
+
+	public static final String CONSENT = "consent";
+
+	public static final String CREDENTIALSUBJECT = "credentialSubject";
+
+	public static final String CREDENTIAL = "credential";
+
+	public static final String PROOF = "proof";
+
+	public static final String VERFIABLECREDENTIAL = "VerifiableCredential";
+
+	public static final String MOSIPVERFIABLECREDENTIAL = "MOSIPVerifiableCredential";
+
+	public static final String SIGNATURE = "signature";
+}
