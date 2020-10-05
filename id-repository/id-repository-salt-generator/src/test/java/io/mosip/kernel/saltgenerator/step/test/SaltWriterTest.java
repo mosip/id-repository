adc0f95cd898d9deb63a30fb0eb88af76e39a02d
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-10-05 10:28:10 +0530

    Disable test

--- id-repository/id-repository-salt-generator/src/test/java/io/mosip/kernel/saltgenerator/step/test/SaltWriterTest.java
+++ id-repository/id-repository-salt-generator/src/test/java/io/mosip/kernel/saltgenerator/step/test/SaltWriterTest.java
@@ -3,7 +3,7 @@ package io.mosip.kernel.saltgenerator.step.test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class SaltWriterTest {
 
 //	@InjectMocks
