3eba125be82c1a9fb97399583b2ba9a0e33eec73
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-10-12 20:29:09 +0530

    MOSIP-8547 retry subscription added

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/init/SubscribeEvent.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/init/SubscribeEvent.java
@@ -1,45 +1,30 @@
 package io.mosip.credential.request.generator.init;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
-import io.mosip.credential.request.generator.batch.config.CredentialItemProcessor;
-import io.mosip.idrepository.core.dto.EventModel;
+import io.mosip.credential.request.generator.integration.WebSubSubscriptionHelper;
 import io.mosip.idrepository.core.logger.IdRepoLogger;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
 import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.core.websub.spi.PublisherClient;
-import io.mosip.kernel.core.websub.spi.SubscriptionClient;
-import io.mosip.kernel.websub.api.exception.WebSubClientException;
-import io.mosip.kernel.websub.api.model.SubscriptionChangeRequest;
-import io.mosip.kernel.websub.api.model.SubscriptionChangeResponse;
-import io.mosip.kernel.websub.api.model.UnsubscriptionRequest;
 
 @Component
 public class SubscribeEvent implements ApplicationListener<ApplicationReadyEvent> {
 
 	@Autowired
-	SubscriptionClient<SubscriptionChangeRequest, UnsubscriptionRequest, SubscriptionChangeResponse> sb;
+	private ThreadPoolTaskScheduler taskScheduler;
 
-	@Value("${WEBSUBSUBSCRIBEURL}")
-	private String webSubHubUrl;
-
-	@Value("${WEBSUBSECRET}")
-	private String webSubSecret;
-
-	@Value("${CALLBACKURL}")
-	private String callBackUrl;
-
-	/** The config server file storage URL. */
-	@Value("${mosip.partnerhuburl}")
-	private String partnerhuburl;
+	@Value("${subscription-delay-secs:60000}")
+	private int taskSubsctiptionDelay;
 
 	@Autowired
-	private PublisherClient<String, EventModel, HttpHeaders> pb;
+	private WebSubSubscriptionHelper webSubSubscriptionHelper;
 
 	/** The Constant BIOMETRICS. */
 	private static final String ONAPPLICATIONEVENT = "onApplicationEvent";
@@ -47,32 +32,21 @@ public class SubscribeEvent implements ApplicationListener<ApplicationReadyEvent
 	/** The Constant ID_REPO_SERVICE_IMPL. */
 	private static final String SUBSCIRBEEVENT = "SubscribeEvent";
 
-	private static final Logger LOGGER = IdRepoLogger.getLogger(CredentialItemProcessor.class);
+	private static final Logger LOGGER = IdRepoLogger.getLogger(SubscribeEvent.class);
 
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
-		registerTopic();
-		try {
-			SubscriptionChangeRequest subscriptionRequest = new SubscriptionChangeRequest();
-			subscriptionRequest.setCallbackURL(callBackUrl);
-			subscriptionRequest.setHubURL(webSubHubUrl);
-			subscriptionRequest.setSecret(webSubSecret);
-			subscriptionRequest.setTopic("CREDENTIAL_STATUS_UPDATE");
-			sb.subscribe(subscriptionRequest);
-		} catch (WebSubClientException e) {
-			LOGGER.info(IdRepoSecurityManager.getUser(), SUBSCIRBEEVENT, ONAPPLICATIONEVENT,
-					"websub subscription error");
-		}
+		LOGGER.info(IdRepoSecurityManager.getUser(), SUBSCIRBEEVENT, ONAPPLICATIONEVENT,
+				"Scheduling event subscriptions after (milliseconds): " + taskSubsctiptionDelay);
+
+		taskScheduler.schedule(this::initSubsriptions, new Date(System.currentTimeMillis() + taskSubsctiptionDelay));
 
 	}
 
-	private void registerTopic() {
-		try {
-			pb.registerTopic("CREDENTIAL_STATUS_UPDATE", partnerhuburl);
-		} catch (WebSubClientException e) {
-			LOGGER.info(IdRepoSecurityManager.getUser(), SUBSCIRBEEVENT, ONAPPLICATIONEVENT,
-					"topic already registered");
-		}
+	private void initSubsriptions() {
+		LOGGER.info(IdRepoSecurityManager.getUser(), SUBSCIRBEEVENT, ONAPPLICATIONEVENT,
+				"Initializing subscribptions..");
 
+		webSubSubscriptionHelper.initSubsriptions();
 	}
 }
