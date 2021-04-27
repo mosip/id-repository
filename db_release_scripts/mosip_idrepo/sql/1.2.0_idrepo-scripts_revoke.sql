dd09e63030a5fe66a3201af63c48623d89f58775
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-04-27 15:38:27 +0530

    MOSIP-13528 Added unlock timestamp to uin_auth_lock table

--- db_release_scripts/mosip_idrepo/sql/1.2.0_idrepo-scripts_revoke.sql
+++ db_release_scripts/mosip_idrepo/sql/1.2.0_idrepo-scripts_revoke.sql
@@ -2,10 +2,13 @@
 -- Database Name: mosip_idrepo
 -- Release Version 	: 1.2
 -- Purpose    		: Revoking Database Alter deployement done for release in ID Repository DB.       
--- Create By   		: Sadanandegowda DM
--- Created Date		: Sep-2020
+-- Create By   		: Ram Bhatt
+-- Created Date		: Apr-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
 
 \c mosip_idrepo sysadmin
+
+DROP TABLE IF EXISTS idrepo.uin_auth_lock;
+-----------------------------------------------------------------------------------------------------
