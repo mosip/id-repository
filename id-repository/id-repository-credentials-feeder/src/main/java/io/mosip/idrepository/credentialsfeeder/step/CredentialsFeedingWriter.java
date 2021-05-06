7e30cf2286ed01f1a9a6f7b41b9b6f1738162ac9
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-05-06 19:50:33 +0530

    Initial commit.

---
+++ id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/step/CredentialsFeedingWriter.java
@@ -0,0 +1,35 @@
+package io.mosip.idrepository.credentialsfeeder.step;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import io.mosip.idrepository.credentialsfeeder.entity.idrepo.CredentialRequestEntity;
+import io.mosip.idrepository.credentialsfeeder.logger.IdRepoLogger;
+import io.mosip.kernel.core.logger.spi.Logger;
+
+/**
+ * The Class CredentialsFeedingWriter - Class to feed credentials using credential requests.
+ * Implements {@code ItemWriter}.
+ *
+ * @author Manoj SP
+ */
+@Component
+public class CredentialsFeedingWriter implements ItemWriter<CredentialRequestEntity> {
+
+	Logger mosipLogger = IdRepoLogger.getLogger(CredentialsFeedingWriter.class);
+
+
+	/* (non-Javadoc)
+	 * @see org.springframework.batch.item.ItemWriter#write(java.util.List)
+	 */
+	@Override
+	@Transactional
+	public void write(List<? extends CredentialRequestEntity> entitiesCompositeList) throws Exception {
+		
+	}
+
+}
