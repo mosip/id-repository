e27b329165e9e04353f06f6d0a59714e3f196fe1
Chandrashekar <chandrashekar.shetti@technoforte.co.in>
2021-05-07 16:46:28 +0530

    Update 1.2.0_idrepo-scripts_release.sql
    Alter table scripts

--- db_release_scripts/mosip_idrepo/sql/1.2.0_idrepo-scripts_release.sql
+++ db_release_scripts/mosip_idrepo/sql/1.2.0_idrepo-scripts_release.sql
@@ -3,7 +3,7 @@
 -- Release Version 	: 1.2
 -- Purpose    		: Database Alter scripts for the release for ID Repository DB.       
 -- Create By   		: Ram Bhatt
--- Created Date		: Apr-2021
+-- Created Date		: May-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
@@ -11,10 +11,6 @@
 
 \c mosip_idrepo sysadmin
 
-
-DROP TABLE IF EXISTS idrepo.uin_auth_lock;
-
-\ir ../ddl/idrepo-uin_auth_lock.sql
 
 ---------------------------------------------------------------------------------------------------
 
