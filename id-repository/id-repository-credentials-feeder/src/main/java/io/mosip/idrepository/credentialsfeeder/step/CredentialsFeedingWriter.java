1c6f601430cb631dcb0ef63a5955ea9906835356
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-05-10 17:00:44 +0530

    WIP updated for send notification

--- id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/step/CredentialsFeedingWriter.java
+++ id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/step/CredentialsFeedingWriter.java
@@ -1,12 +1,8 @@
 package io.mosip.idrepository.credentialsfeeder.step;
 
-import static io.mosip.idrepository.core.constant.IdRepoConstants.MODULO_VALUE;
-
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.batch.item.ItemWriter;
@@ -15,20 +11,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import io.mosip.idrepository.core.constant.IdType;
-import io.mosip.idrepository.core.constant.RestServicesConstants;
 import io.mosip.idrepository.core.dto.CredentialIssueRequestDto;
-import io.mosip.idrepository.core.dto.CredentialIssueRequestWrapperDto;
-import io.mosip.idrepository.core.exception.IdRepoDataValidationException;
-import io.mosip.idrepository.core.exception.RestServiceException;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
+import io.mosip.idrepository.core.util.CredentialRequestManager;
 import io.mosip.idrepository.credentialsfeeder.entity.idrepo.CredentialRequestStatusEntity;
 import io.mosip.idrepository.credentialsfeeder.logger.IdRepoLogger;
 import io.mosip.idrepository.credentialsfeeder.repository.idrepo.UinHashSaltRepo;
 import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.core.util.DateUtils;
 
 /**
  * The Class CredentialsFeedingWriter - Class to feed credentials using credential requests.
@@ -40,19 +29,6 @@ import io.mosip.kernel.core.util.DateUtils;
 public class CredentialsFeedingWriter implements ItemWriter<CredentialRequestStatusEntity> {
 
 	private static final Logger mosipLogger = IdRepoLogger.getLogger(CredentialsFeedingWriter.class);
-	
-	private static final String TOKEN = "TOKEN";
-
-	private static final String SALT = "SALT";
-
-	private static final String MODULO = "MODULO";
-
-	private static final String ID_HASH = "id_hash";
-
-	private static final String EXPIRY_TIMESTAMP = "expiry_timestamp";
-
-	private static final String TRANSACTION_LIMIT = "transaction_limit";
-	
 	
 	@Value("${online-verification-partner-ids}")
 	private List<String> onlineVerificationPartnerIds;
@@ -66,7 +42,7 @@ public class CredentialsFeedingWriter implements ItemWriter<CredentialRequestSta
 	private IdRepoSecurityManager securityManager;
 	
 	@Autowired
-	private Environment env;
+	private CredentialRequestManager credentialRequestManager;
 
 
 	/* (non-Javadoc)
@@ -74,77 +50,32 @@ public class CredentialsFeedingWriter implements ItemWriter<CredentialRequestSta
 	 */
 	@Override
 	public void write(List<? extends CredentialRequestStatusEntity> requestIdEntitiess) throws Exception {
-		
+		sendEventsToCredService(requestIdEntitiess, onlineVerificationPartnerIds);
 	}
 	
-	private void sendEventsToCredService(List<CredentialRequestStatusEntity> requestEntities, boolean isUpdate,
+	private void sendEventsToCredService(List<? extends CredentialRequestStatusEntity> requestEntities,
 			 List<String> partnerIds) {
 		if (requestEntities != null) {
-			List<CredentialIssueRequestDto> vidRequests = requestEntities.stream().flatMap(entity -> {
+			List<CredentialIssueRequestDto> requests = requestEntities.stream().flatMap(entity -> {
 				return partnerIds.stream().map(partnerId -> {
-					String token = tokenIDGenerator.generateTokenID(uin, partnerId);
-					return createCredReqDto(entity.getVid(), partnerId, entity.getIdExpiryDtimes(),
-							entity.getIdTransactionLimit(), token, IdType.VID.getIdType(),
-							entity.getHashAttributes());
+					return createCredReqDto(entity.getIndividualId(), partnerId, entity.getIdExpiryDtimes(),
+							entity.getIdTransactionLimit(), entity.getTokenId(),
+							securityManager.getIdHashAndAttributes(entity.getIndividualId(), uinHashSaltRepo::retrieveSaltById));
 				});
 			}).collect(Collectors.toList());
-			eventRequestsList.addAll(vidRequests);
+			
+			credentialRequestManager.sendRequestToCredService(requests, false, (req, res) -> {
+				
+			});
 		}
-
-		eventRequestsList.forEach(reqDto -> {
-			CredentialIssueRequestWrapperDto requestWrapper = new CredentialIssueRequestWrapperDto();
-			requestWrapper.setRequest(reqDto);
-			requestWrapper.setRequesttime(DateUtils.getUTCCurrentDateTime());
-			String eventTypeDisplayName = isUpdate ? "Update ID" : "Create ID";
-			mosipLogger.info(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "notify",
-					"notifying Credential Service for event " + eventTypeDisplayName);
-			sendRequestToCredService(requestWrapper);
-			mosipLogger.info(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "notify",
-					"notified Credential Service for event" + eventTypeDisplayName);
-		});
-	}
-
-	private void sendRequestToCredService(CredentialIssueRequestWrapperDto requestWrapper) {
-		try {
-			Map<String, Object> response = restHelper.requestSync(restBuilder
-					.buildRequest(RestServicesConstants.CREDENTIAL_REQUEST_SERVICE, requestWrapper, Map.class));
-			mosipLogger.info(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "sendRequestToCredService",
-					"Response of Credential Request: " + mapper.writeValueAsString(response));
-		} catch (RestServiceException | IdRepoDataValidationException | JsonProcessingException e) {
-			mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "sendRequestToCredService",
-					e.getMessage());
-		}
+		
 	}
 
 	private CredentialIssueRequestDto createCredReqDto(String id, String partnerId, LocalDateTime expiryTimestamp,
-			Integer transactionLimit, String token, String idType,
+			Integer transactionLimit, String token,
 			Map<? extends String, ? extends Object> idHashAttributes) {
-		Map<String, Object> data = new HashMap<>();
-		data.putAll(idHashAttributes);
-		data.put(EXPIRY_TIMESTAMP, Optional.ofNullable(expiryTimestamp).map(DateUtils::formatToISOString).orElse(null));
-		data.put(TRANSACTION_LIMIT, transactionLimit);
-		data.put(TOKEN, token);
-
-		CredentialIssueRequestDto credentialIssueRequestDto = new CredentialIssueRequestDto();
-		credentialIssueRequestDto.setId(id);
-		credentialIssueRequestDto.setCredentialType(credentialType);
-		credentialIssueRequestDto.setIssuer(partnerId);
-		credentialIssueRequestDto.setRecepiant(credentialRecepiant);
-		credentialIssueRequestDto.setUser(IdRepoSecurityManager.getUser());
-		credentialIssueRequestDto.setAdditionalData(data);
-		return credentialIssueRequestDto;
+		return credentialRequestManager.createCredReqDto(id, partnerId, expiryTimestamp, transactionLimit, token, idHashAttributes);
 	}
 	
-	private Map<String, String> getIdHashAndAttributes(String id) {
-		Map<String, String> hashWithAttributes = new HashMap<>();
-		Integer moduloValue = env.getProperty(MODULO_VALUE, Integer.class);
-		int modResult = (int) (Long.parseLong(id) % moduloValue);
-		String hashSalt = uinHashSaltRepo.retrieveSaltById(modResult);
-		String hash = securityManager.hashwithSalt(id.getBytes(), hashSalt.getBytes());
-		hashWithAttributes.put(ID_HASH, hash);
-		hashWithAttributes.put(MODULO, String.valueOf(modResult));
-		hashWithAttributes.put(SALT, hashSalt);
-		return hashWithAttributes;
-	}
-
+	
 }
