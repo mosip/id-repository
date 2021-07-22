b0ba4921e27319e20383fb32bf1977e2c01a418b
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-07-22 17:27:01 +0530

    UIN business draft table
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- db_release_scripts/mosip_idrepo/ddl/idrepo-uin_biometric_draft.sql
+++ db_release_scripts/mosip_idrepo/ddl/idrepo-uin_biometric_draft.sql
@@ -14,20 +14,19 @@
 -- object: idrepo.uin_draft | type: TABLE --
 -- DROP TABLE IF EXISTS idrepo.uin_draft CASCADE;
 CREATE TABLE idrepo.uin_draft(
-	reg_id character varying (39) NOT NULL,
-	uin character varying (500) NOT NULL,
-	uin_hash character varying (128) NOT NULL,
-	uin_data bytea,		
-	uin_data_hash character varying (64),			
-	status_code character varying (32) NOT NULL,	
-	cr_by character varying (256) NOT NULL,		
-	cr_dtimes timestamp NOT NULL,		
-	upd_by	character varying (256),			
-	upd_dtimes timestamp,			
-	is_deleted bool	DEFAULT FALSE,
-	del_dtimes timestamp FALSE,
-	CONSTRAINT pk_uindft_id PRIMARY KEY (reg_id),
-	CONSTRAINT unq_uin UNIQUE (uin),
+	reg_id character varying(39) NOT NULL,								
+	biometric_file_type character varying(36) NOT NULL,						
+	bio_file_id character varying(128) NOT NULL,					
+	biometric_file_name character varying(128) NOT NULL,						
+	biometric_file_hash character varying(64) NOT NULL,					
+	cr_by character varying(256) NOT NULL,		
+	cr_dtimes timestamp NOT NULL,			
+	upd_by character varying(256),		
+	upd_dtimes timestamp,	
+	is_deleted bool	DEFAULT	FALSE,
+	del_dtimes timestamp,														
+	CONSTRAINT pk_uinbiodft_id PRIMARY KEY (reg_id,biometric_file_type),
+	CONSTRAINT unq_biofileid UNIQUE (bio_file_id),
 	CONSTRAINT unq_uinhsh UNIQUE (uin_hash)
 );
 -- ddl-end --
