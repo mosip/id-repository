721b33ed3410249548adb00466a377cb98741936
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-12-29 09:56:50 +0530

    Index Implementation

--- db_release_scripts/mosip_credential/sql/1.2.0_credential-scripts_release.sql
+++ db_release_scripts/mosip_credential/sql/1.2.0_credential-scripts_release.sql
@@ -9,3 +9,8 @@
 -- -------------------------------------------------------------------------------------------------
 
 \c mosip_credential sysadmin
+
+CREATE INDEX IF NOT EXISTS idx_cred_trn_cr_dtimes ON credential.credential_transaction USING btree (cr_dtimes);
+
+
+---------------------------------------------------------------------------------------------------
