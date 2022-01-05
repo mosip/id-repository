bd038a8a3dc84ec1e052c68dea5f71119cac794d
Manoj SP <Manoj.SP@mindtree.com>
2022-01-05 09:44:30 +0530

    [MOSIP-18655] updated rest builder to be initiated from config

--- id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/VidBootApplication.java
+++ id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/VidBootApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 
-import io.mosip.idrepository.core.builder.RestRequestBuilder;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
 import io.mosip.idrepository.core.util.DummyPartnerCheckUtil;
 import io.mosip.kernel.crypto.jce.core.CryptoCore;
@@ -18,8 +17,7 @@ import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
  * @author Prem Kumar
  */
 @SpringBootApplication(exclude = HibernateDaoConfig.class)
-@Import({ RestRequestBuilder.class, IdRepoSecurityManager.class, DummyPartnerCheckUtil.class,
-		CryptoCore.class })
+@Import({ IdRepoSecurityManager.class, DummyPartnerCheckUtil.class, CryptoCore.class })
 @ComponentScan(basePackages = { "io.mosip.idrepository.vid.*", "io.mosip.idrepository.core.*",
 		"${mosip.auth.adapter.impl.basepackage}" }, excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = {
 				"io.mosip.idrepository.core.entity", "io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig" }))
