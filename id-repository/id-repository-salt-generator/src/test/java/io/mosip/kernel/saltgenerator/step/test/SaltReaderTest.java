495f82eb5c68aefa05b1a76f162e9cf6f39954a7
Manoj SP <Manoj.SP@mindtree.com>
2022-01-27 14:40:40 +0530

    [MOSIP-9799] updated salt reader and writer test cases

--- id-repository/id-repository-salt-generator/src/test/java/io/mosip/kernel/saltgenerator/step/test/SaltReaderTest.java
+++ id-repository/id-repository-salt-generator/src/test/java/io/mosip/kernel/saltgenerator/step/test/SaltReaderTest.java
@@ -1,27 +1,39 @@
 package io.mosip.kernel.saltgenerator.step.test;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant;
+import io.mosip.idrepository.saltgenerator.entity.IdRepoSaltEntitiesComposite;
+import io.mosip.idrepository.saltgenerator.step.SaltReader;
+
 public class SaltReaderTest {
 
-//	SaltReader saltReader = new SaltReader();
-//
-//	MockEnvironment mockEnv = new MockEnvironment();
-//
-//	@Test
-//	public void testRead() {
-//		mockEnv.setProperty(SaltGeneratorConstant.START_SEQ.getValue(), String.valueOf(0));
-//		mockEnv.setProperty(SaltGeneratorConstant.END_SEQ.getValue(), String.valueOf(1));
-//		ReflectionTestUtils.setField(saltReader, "env", mockEnv);
-//		saltReader.initialize();
-//		SaltEntity saltEntity = saltReader.read();
-//		assertTrue(saltEntity.getId().equals(0l));
-//	}
-//	
-//	@Test
-//	public void testReadNull() {
-//		mockEnv.setProperty(SaltGeneratorConstant.START_SEQ.getValue(), String.valueOf(1));
-//		mockEnv.setProperty(SaltGeneratorConstant.END_SEQ.getValue(), String.valueOf(0));
-//		ReflectionTestUtils.setField(saltReader, "env", mockEnv);
-//		saltReader.initialize();
-//		assertNull(saltReader.read());
-//	}
+	SaltReader saltReader = new SaltReader();
+
+	MockEnvironment mockEnv = new MockEnvironment();
+	
+	private void init(int start, int end) {
+		mockEnv.setProperty(SaltGeneratorConstant.START_SEQ.getValue(), String.valueOf(start));
+		mockEnv.setProperty(SaltGeneratorConstant.END_SEQ.getValue(), String.valueOf(end));
+		ReflectionTestUtils.setField(saltReader, "env", mockEnv);
+		saltReader.initialize();
+	}
+
+	@Test
+	public void testRead() {
+		init(0, 1);
+		IdRepoSaltEntitiesComposite saltEntity = saltReader.read();
+		assertTrue(saltEntity.getIdentityEncryptSaltEntity().getId().equals(0l));
+	}
+	
+	@Test
+	public void testReadNull() {
+		init(1, 0);
+		assertNull(saltReader.read());
+	}
 }
