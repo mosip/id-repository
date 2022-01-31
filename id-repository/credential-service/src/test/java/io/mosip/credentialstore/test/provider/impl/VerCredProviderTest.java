62c62c07b91c87ef032c1d16493413eabf93f4de
Aiham Hasan <Aiham.Hasan@mindtree.com>
2022-01-31 16:31:02 +0530

    Added new test cases
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- id-repository/credential-service/src/test/java/io/mosip/credentialstore/test/provider/impl/VerCredProviderTest.java
+++ id-repository/credential-service/src/test/java/io/mosip/credentialstore/test/provider/impl/VerCredProviderTest.java
@@ -1,6 +1,6 @@
 package io.mosip.credentialstore.test.provider.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.HashMap;
 import java.util.List;
@@ -8,12 +8,10 @@ import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -26,12 +24,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import foundation.identity.jsonld.ConfigurableDocumentLoader;
 import io.mosip.credentialstore.dto.AllowedKycDto;
+import io.mosip.credentialstore.dto.DataProviderResponse;
 import io.mosip.credentialstore.exception.CredentialFormatterException;
 import io.mosip.credentialstore.provider.impl.VerCredProvider;
 import io.mosip.credentialstore.util.DigitalSignatureUtil;
 import io.mosip.credentialstore.util.EncryptionUtil;
 import io.mosip.credentialstore.util.Utilities;
-import io.mosip.idrepository.core.constant.IdRepoConstants;
 import io.mosip.idrepository.core.dto.CredentialServiceRequestDto;
 import io.mosip.idrepository.core.util.EnvUtil;
 
@@ -96,25 +94,40 @@ public class VerCredProviderTest {
 		ReflectionTestUtils.setField(verCredProvider, "verCredIssuer", "demo");
 		ReflectionTestUtils.setField(verCredProvider, "proofPurpose", "test");
 		ReflectionTestUtils.setField(verCredProvider, "verificationMethod", "test");
-		ReflectionTestUtils.setField(env, "dateTimePattern", "test");
+		ReflectionTestUtils.setField(env, "dateTimePattern", "yyyy-MM-ddHH:mm:ss");
 	}
 
-	@Ignore
 	@Test
 	public void getFormattedCredentialDataTest() throws CredentialFormatterException {
 		CredentialServiceRequestDto credentialServiceRequestDto = new CredentialServiceRequestDto();
 		Map<String, Object> additionalData = new HashMap<>();
+		additionalData.put("key1", "value1");
 		credentialServiceRequestDto.setAdditionalData(additionalData);
+		credentialServiceRequestDto.setEncryptionKey("Test");
+		credentialServiceRequestDto.setId("02");
 		Map<AllowedKycDto, Object> sharableAttributes = new HashMap<>();
-		verCredProvider.getFormattedCredentialData(credentialServiceRequestDto, sharableAttributes);
+		AllowedKycDto dto = new AllowedKycDto();
+		dto.setGroup("Test");
+		dto.setEncrypted(true);
+		dto.setAttributeName("Test");
+		sharableAttributes.put(dto, "value");
+		DataProviderResponse response = verCredProvider.getFormattedCredentialData(credentialServiceRequestDto,
+				sharableAttributes);
+		assertNotNull(response);
 	}
 
 	@Test(expected = CredentialFormatterException.class)
-	public void getFormattedCredentialDataExceptionTest() throws CredentialFormatterException {
+	public void getFormattedCredentialDataExceptionTest1() throws CredentialFormatterException {
 		CredentialServiceRequestDto credentialServiceRequestDto = new CredentialServiceRequestDto();
 		Map<String, Object> additionalData = new HashMap<>();
+		additionalData.put("key1", "value1");
 		credentialServiceRequestDto.setAdditionalData(additionalData);
 		Map<AllowedKycDto, Object> sharableAttributes = new HashMap<>();
+		AllowedKycDto dto = new AllowedKycDto();
+		dto.setGroup("Test");
+		dto.setEncrypted(true);
+		dto.setAttributeName("Test");
+		sharableAttributes.put(dto, "value");
 		verCredProvider.getFormattedCredentialData(credentialServiceRequestDto, sharableAttributes);
 	}
 
