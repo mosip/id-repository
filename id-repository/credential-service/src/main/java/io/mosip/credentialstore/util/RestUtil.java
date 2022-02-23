ad925adaa27d0bb45b845ef0cc9fa616b56d63ee
Vipul Dhurve <vipuldhurve.13@gmail.com>
2022-02-23 13:44:37 +0530

    Changed == to .equals()

--- id-repository/credential-service/src/main/java/io/mosip/credentialstore/util/RestUtil.java
+++ id-repository/credential-service/src/main/java/io/mosip/credentialstore/util/RestUtil.java
@@ -143,7 +143,7 @@ public class RestUtil {
                 Iterator<String> iterator = httpHeader.keySet().iterator();
                 while (iterator.hasNext()) {
                     String key = iterator.next();
-                    if (!(headers.containsKey("Content-Type") && key == "Content-Type"))
+                    if (!(headers.containsKey("Content-Type") && key.equals("Content-Type")))
                         headers.add(key, httpHeader.get(key).get(0));
                 }
                 return new HttpEntity<Object>(httpEntity.getBody(), headers);
