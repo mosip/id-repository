b5ed58fb000c7fb335b58f625f9235df2886bbcd
Manoj SP <Manoj.SP@mindtree.com>
2022-04-04 21:25:43 +0530

    [MOSIP-21073] Fixed 1.2.0.1 release scripts

--- db_release_scripts/mosip_credential/sql/1.2.0_credential-scripts_revoke.sql
+++ db_release_scripts/mosip_credential/sql/1.2.0_credential-scripts_revoke.sql
@@ -10,6 +10,4 @@
 
 \c mosip_credential sysadmin
 
-ALTER TABLE credential.credential_transaction DROP COLUMN status_comment ;
-
 -----------------------------------------------------------------------------------------------------
--- db_release_scripts/mosip_idrepo/sql/1.2.0_idrepo-scripts_revoke.sql
+++ db_release_scripts/mosip_idrepo/sql/1.2.0_idrepo-scripts_revoke.sql
@@ -12,17 +12,4 @@
 
 \c mosip_idrepo sysadmin
 
-ALTER TABLE idrepo.uin_auth_lock DROP COLUMN unlock_expiry_datetime;
-DROP TABLE IF EXISTS idrepo.credential_request_status;
------------------------------------------------------------------------------------------------------
-
-
-DROP TABLE IF EXISTS idrepo.uin_biometric_draft;
-DROP TABLE IF EXISTS idrepo.uin_draft;
-DROP TABLE IF EXISTS idrepo.uin_document_draft;
-
-------------------------------------------------------------------------------------------------------
-
-DROP TABLE IF EXISTS idrepo.anonymous_profile;
-DROP TABLE IF EXISTS idrepo.channel_info;
 ------------------------------------------------------------------------------------------------------
