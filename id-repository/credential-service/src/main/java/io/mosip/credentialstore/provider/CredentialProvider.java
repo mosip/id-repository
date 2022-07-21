af35cd523b282e67ade1f0d5b8fda34c3ed4044f
dhanendra06 <60607841+dhanendra06@users.noreply.github.com>
2022-07-21 15:24:06 +0530

    MOSIP-23106 (#660)
    * MOSIP-23106
    
    * updated code based on review comments
    
    * updated code based on review comments

--- id-repository/credential-service/src/main/java/io/mosip/credentialstore/provider/CredentialProvider.java
+++ id-repository/credential-service/src/main/java/io/mosip/credentialstore/provider/CredentialProvider.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import io.mosip.credentialstore.util.VIDUtil;
+import io.mosip.idrepository.core.dto.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.mvel2.MVEL;
@@ -41,9 +43,6 @@ import io.mosip.credentialstore.exception.DataEncryptionFailureException;
 import io.mosip.credentialstore.util.EncryptionUtil;
 import io.mosip.credentialstore.util.JsonUtil;
 import io.mosip.credentialstore.util.Utilities;
-import io.mosip.idrepository.core.dto.CredentialServiceRequestDto;
-import io.mosip.idrepository.core.dto.DocumentsDTO;
-import io.mosip.idrepository.core.dto.IdResponseDTO;
 import io.mosip.idrepository.core.logger.IdRepoLogger;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
 import io.mosip.kernel.core.cbeffutil.entity.BDBInfo;
@@ -83,6 +82,9 @@ public class CredentialProvider {
 	@Autowired
 	private CbeffUtil cbeffutil;
 
+	@Autowired
+	VIDUtil vidUtil;
+
 	@Autowired(required = true)
 	@Qualifier("varres")
 	VariableResolverFactory functionFactory;
@@ -92,6 +94,9 @@ public class CredentialProvider {
 
 	@Value("${credential.service.dob.format}")
 	private String dobFormat;
+
+	@Value("${credential.service.default.vid.type:PERPETUAL}")
+	private String defaultVidType;
 
 	private static final Logger LOGGER = IdRepoLogger.getLogger(CredentialProvider.class);
 	/**
@@ -247,6 +252,24 @@ public class CredentialProvider {
 						attributesMap.put(key, formattedObject);
 					} else if (attribute.equalsIgnoreCase(CredentialConstants.ENCRYPTIONKEY)) {
 						additionalData.put(key.getAttributeName(), credentialServiceRequestDto.getEncryptionKey());
+					}else if(attribute.equalsIgnoreCase(CredentialConstants.VID)){
+						VidInfoDTO vidInfoDTO;
+						VidResponseDTO vidResponseDTO;
+						String vidType= key.getSource().get(0).getFilter().get(0).getType()==null?defaultVidType:key.getSource().get(0).getFilter().get(0).getType();
+						if(key.getFormat().equalsIgnoreCase(CredentialConstants.RETRIEVE)) {
+							vidInfoDTO = vidUtil.getVIDData(identity.get("UIN").toString(), vidType,null);
+							if (vidInfoDTO == null) {
+								vidResponseDTO=vidUtil.generateVID(identity.get("UIN").toString(), vidType);
+								vidInfoDTO = vidUtil.getVIDData(identity.get("UIN").toString(), vidType,vidResponseDTO.getVid());
+							}
+						}else {
+							vidResponseDTO=vidUtil.generateVID(identity.get("UIN").toString(), vidType);
+							vidInfoDTO = vidUtil.getVIDData(identity.get("UIN").toString(),vidType,vidResponseDTO.getVid());
+						}
+						attributesMap.put(key, vidInfoDTO.getVid());
+						additionalData.put("ExpiryTimestamp", vidInfoDTO.getExpiryTimestamp());
+						additionalData.put("TransactionLimit", vidInfoDTO.getTransactionLimit());
+
 					}
 			}
 		}
