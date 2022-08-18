8ed0e37271f4993b6352c154d59a23fb7dcc8b8a
kameshsr <kameshsr1338@gmail.com>
2022-08-18 18:12:47 +0530

    release file name changes.
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- db_release_scripts/mosip_idrepo/sql/1.2.0_revoke.sql
+++ db_release_scripts/mosip_idrepo/sql/1.2.0_revoke.sql
@@ -1,11 +1,28 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_idrepo
--- Release Version 	: 1.1.2
+-- Release Version 	: 1.2
 -- Purpose    		: Revoking Database Alter deployement done for release in ID Repository DB.       
--- Create By   		: Sadanandegowda DM
--- Created Date		: Sep-2020
+-- Create By   		: Ram Bhatt
+-- Created Date		: Apr-2021
 -- 
--- Modified Date        Modified By         Comments / Remarks
+-- Modified Date       Modified By       Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
+-- Sep 2021            Manoj SP           Added anonymous_profile and channel_info tables
+-----------------------------------------------------------------------------------------------------
 
 \c mosip_idrepo sysadmin
+
+ALTER TABLE idrepo.uin_auth_lock DROP COLUMN unlock_expiry_datetime;
+DROP TABLE IF EXISTS idrepo.credential_request_status;
+-----------------------------------------------------------------------------------------------------
+
+
+DROP TABLE IF EXISTS idrepo.uin_biometric_draft;
+DROP TABLE IF EXISTS idrepo.uin_draft;
+DROP TABLE IF EXISTS idrepo.uin_document_draft;
+
+------------------------------------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS idrepo.anonymous_profile;
+DROP TABLE IF EXISTS idrepo.channel_info;
+------------------------------------------------------------------------------------------------------
