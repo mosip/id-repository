394083bc718da32735d59195762cba272ebb3846
kameshsr <kameshsr1338@gmail.com>
2022-08-18 18:12:47 +0530

    release file name changes.

--- db_release_scripts/mosip_idmap/sql/1.2.0_release.sql
+++ db_release_scripts/mosip_idmap/sql/1.2.0_release.sql
@@ -1,6 +1,6 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_idamap
--- Release Version 	: 1.1.2
+-- Release Version 	: 1.2
 -- Purpose    		: Database Alter scripts for the release for ID Map DB.       
 -- Create By   		: Sadanandegowda DM
 -- Created Date		: Sep-2020
