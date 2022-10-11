6550dfd58d56b40793254f978d39f701a5501de7
Neha2365 <110969715+Neha2365@users.noreply.github.com>
2022-10-11 16:38:13 +0530

    Update VidServiceImpl.java (#715)
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/service/impl/VidServiceImpl.java
+++ id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/service/impl/VidServiceImpl.java
@@ -612,7 +612,7 @@ public class VidServiceImpl implements VidService<VidRequestDTO, ResponseWrapper
 	}
 
 	private VidInfoDTO createVidInfo(Vid vid, Map<String, String> idHashAttributes) {
-		return new VidInfoDTO(vid.getVid(),maskData(vid.getVid()), vid.getVidTypeCode(),  vid.getGeneratedDTimes(), vid.getExpiryDTimes(),
+		return new VidInfoDTO(vid.getVid(),maskData(vid.getVid()), vid.getVidTypeCode(),  vid.getExpiryDTimes(), vid.getGeneratedDTimes(),
 				policyProvider.getPolicy(vid.getVidTypeCode()).getAllowedTransactions(),
 				idHashAttributes);
 	}
