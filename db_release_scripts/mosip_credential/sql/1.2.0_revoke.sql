9272c1ff7eae7b4197a33e16b07b3640e2b9e327
ckm007 <chandrakeshavmishra@gmail.com>
2022-10-15 11:25:59 +0530

    [MOSIP-23833] removed not needed ALter script

--- db_release_scripts/mosip_credential/sql/1.2.0_revoke.sql
+++ db_release_scripts/mosip_credential/sql/1.2.0_revoke.sql
@@ -1,3 +1,2 @@
 \c mosip_credential sysadmin
 
-ALTER TABLE credential.credential_transaction DROP COLUMN status_comment ;
--- db_release_scripts/mosip_credential/sql/1.2.0_upgrade.sql
+++ db_release_scripts/mosip_credential/sql/1.2.0_upgrade.sql
@@ -1,3 +1,2 @@
 \c mosip_credential sysadmin
 
-ALTER TABLE credential.credential_transaction ADD COLUMN status_comment character varying(512) ;
