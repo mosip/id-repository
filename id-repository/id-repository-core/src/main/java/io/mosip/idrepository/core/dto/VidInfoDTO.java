aaa9520ed448816349be800f24ee7aaca620e7ad
Neha2365 <110969715+Neha2365@users.noreply.github.com>
2022-12-14 17:01:19 +0530

    Removed Masked VID  (#747)
    * resolved the MOSIP ticket 22141
    
    * bug fixed
    
    * Removed Masked VID
    
    * Resolved merge conflict
    
    Co-authored-by: Neha Farheen <m1092365@mindtree.com>

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/dto/VidInfoDTO.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/dto/VidInfoDTO.java
@@ -17,7 +17,6 @@ import lombok.NoArgsConstructor;
 public class VidInfoDTO {
 
 	private String vid;
-	private String maskedVid;
 	private String vidType;
 	private LocalDateTime expiryTimestamp;
 	private LocalDateTime genratedOnTimestamp;
