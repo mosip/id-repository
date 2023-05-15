b863b33508e1fb89eec9f56052656661c034c580
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-05-15 15:15:49 +0530

    Mosip 23833 (#783)
    * [MOSIP-23833] renamed release scripts
    
    * [MOSIP-23833] updated release scripts
    
    * [MOSIP-23833] removed not needed ALter script
    
    * [MOSIP-23833] removed not needed ALter script
    
    * [MOSIP-23833] added psql error handling
    
    * [MOSIP-23422] changes for including patch release
    
    * [MOSIP-23833] restructered upgrade scripts
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    ---------
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>

--- db_upgrade_scripts/mosip_idrepo/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
@@ -1,14 +1,3 @@
--- -------------------------------------------------------------------------------------------------
--- Database Name: mosip_idrepo
--- Release Version 	: 1.2
--- Purpose    		: Revoking Database Alter deployement done for release in ID Repository DB.       
--- Create By   		: Ram Bhatt
--- Created Date		: Apr-2021
--- 
--- Modified Date       Modified By       Comments / Remarks
--- -------------------------------------------------------------------------------------------------
--- Sep 2021            Manoj SP           Added anonymous_profile and channel_info tables
------------------------------------------------------------------------------------------------------
 
 \c mosip_idrepo sysadmin
 
