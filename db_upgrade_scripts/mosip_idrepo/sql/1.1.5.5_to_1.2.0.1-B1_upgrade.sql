be370322b3e1d5152818a6bf4e2beb519bb885c3
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-05-15 15:15:49 +0530

    Mosip 23833 (#783)
    * [MOSIP-23833] renamed release scripts
    
    * [MOSIP-23833] updated release scripts
    
    * [MOSIP-23833] removed not needed ALter script
    
    * [MOSIP-23833] removed not needed ALter script
    
    * [MOSIP-23833] added psql error handling
    
    * [MOSIP-23422] changes for including patch release
    
    * [MOSIP-23833] restructered upgrade scripts
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    ---------
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- db_upgrade_scripts/mosip_idrepo/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
@@ -1,18 +1,3 @@
--- -------------------------------------------------------------------------------------------------
--- Database Name: mosip_idrepo
--- Release Version 	: 1.2
--- Purpose    		: Database Alter scripts for the release for ID Repository DB.       
--- Create By   		: Ram Bhatt
--- Created Date		: May-2021
--- 
--- Modified Date        Modified By         Comments / Remarks
--- -------------------------------------------------------------------------------------------------
--- Jul-2021		Ram Bhatt	    Creation of uin biometric draft and uin draft tables
--- Jul-2021		Ram Bhatt	    Lang Code nullable for multiple tables
--- Jul-2021		Manoj SP	    Addition of anonymous profile column to tables
--- Sep-2021		Manoj SP	    Removed Anonymous Profile alter scripts
--- Sep-2021		Manoj SP	    Added anonymous_profile and channel_info tables.
--------------------------------------------------------------------------------------------------------
 
 \c mosip_idrepo sysadmin
 ---------------------------------------------------------------------------------------------------
