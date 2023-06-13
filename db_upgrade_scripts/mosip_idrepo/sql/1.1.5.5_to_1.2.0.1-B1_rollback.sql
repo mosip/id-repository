5491ab83bd34d4aea5de95670fbfe1ecce511b51
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-06-13 17:32:56 +0530

    [MOSIP-27964] updated upgrade sql's
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- db_upgrade_scripts/mosip_idrepo/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
@@ -1,5 +1,10 @@
+ALTER DATABASE mosip_idrepo OWNER TO postgres;
 
-\c mosip_idrepo sysadmin
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA idrepo TO postgres;
+
+REVOKE ALL PRIVILEGES ON DATABASE mosip_idmap FROM idrepouser, sysadmin;
+
+GRANT SELECT, INSERT, TRUNCATE, REFERENCES, UPDATE, DELETE ON ALL TABLES IN SCHEMA idrepo TO idrepouser;
 
 ALTER TABLE idrepo.uin_auth_lock DROP COLUMN unlock_expiry_datetime;
 DROP TABLE IF EXISTS idrepo.credential_request_status;
