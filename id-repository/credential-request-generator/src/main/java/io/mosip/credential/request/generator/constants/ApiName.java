6983ed7e8b527b25f2c51c314353515ebc16f53e
Neha Farheen <m1092365@mindtree.com>
2023-10-06 17:35:14 +0530

    Change the implementation of postAPI in rest util

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/constants/ApiName.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/constants/ApiName.java
@@ -7,7 +7,21 @@ package io.mosip.credential.request.generator.constants;
  */
 public enum ApiName {
 
-	CRDENTIALSERVICE,
-	KERNELAUDITMANAGER;
+	CRDENTIALSERVICE("CRDENTIALSERVICE"), 
+	KERNELAUDITMANAGER("mosip.idrepo.audit.rest.uri");
+
+	private final String serviceName;
+
+	private ApiName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	private ApiName() {
+		this.serviceName = "";
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
 
 }
