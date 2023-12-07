450f9357478c93508700de06d9ecbfe92b3bab9e
ase-101 <sunkadaeanusha@gmail.com>
2023-12-07 23:04:15 +0530

    ES-422 Fixed review comments
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/validator/RequestValidator.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/validator/RequestValidator.java
@@ -1,5 +1,6 @@
 package io.mosip.credential.request.generator.validator;
 
+import io.mosip.idrepository.core.dto.CredentialIssueRequest;
 import io.mosip.idrepository.core.dto.CredentialIssueRequestDto;
 import io.mosip.idrepository.core.exception.IdRepoAppException;
 import io.mosip.kernel.core.http.RequestWrapper;
@@ -19,7 +20,7 @@ public class RequestValidator {
     private static final String CREDENTIAL_TYPE = "credentialType";
     private static final Object CREDENTIAL_ISSUE_REQUEST_DTO = "credentialIssueRequestDto";
 
-    public void validateRequestGeneratorRequest(RequestWrapper<CredentialIssueRequestDto> requestWrapper) throws IdRepoAppException {
+    public void validateRequestGeneratorRequest(RequestWrapper<CredentialIssueRequest> requestWrapper) throws IdRepoAppException {
         validateAPIRequestToCheckNull(requestWrapper);
         validateDataToCheckNullOrEmpty(requestWrapper.getRequest().getCredentialType(),
                 CREDENTIAL_TYPE);
@@ -27,7 +28,7 @@ public class RequestValidator {
        
     }
 
-    private void validateAPIRequestToCheckNull(RequestWrapper<CredentialIssueRequestDto> requestWrapper) throws IdRepoAppException {
+    private void validateAPIRequestToCheckNull(RequestWrapper<CredentialIssueRequest> requestWrapper) throws IdRepoAppException {
         if (requestWrapper.getRequest() == null) {
             throw new IdRepoAppException(INVALID_INPUT_PARAMETER.getErrorCode(),
                     String.format(INVALID_INPUT_PARAMETER.getErrorMessage(), CREDENTIAL_ISSUE_REQUEST_DTO));
