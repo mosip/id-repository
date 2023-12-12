99b9a5b96f12779d6f4fe086d21bf43c76e22746
ase-101 <sunkadaeanusha@gmail.com>
2023-12-13 01:46:45 +0530

    Review comments fixes
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

---
+++ db_release_scripts/mosip_credential/sql/1.2.1_release.sql
@@ -0,0 +1,17 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_credential
+-- Release Version 	: 1.2
+-- Purpose    		: Database Alter scripts for the release for credential DB.       
+-- Create By   		: Sadanandegowda DM
+-- Created Date		: Sep-2020
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -------------------------------------------------------------------------------------------------
+-- Apr-2021 		Ram Bhatt 	    status_comment column added.
+----------------------------------------------------------------------------------------------------
+
+\c mosip_credential sysadmin
+
+ALTER TABLE credential.credential_transaction ALTER COLUMN id type character varying(64);
+
+---------------------------------------------------------------------------------------------------
