f3ae268a5eed813918bf9f8144fd36cce167f829
ase-101 <sunkadaeanusha@gmail.com>
2023-12-13 13:37:12 +0530

    Fixed review comments
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- db_release_scripts/mosip_credential/sql/1.2.1_revoke.sql
+++ db_release_scripts/mosip_credential/sql/1.2.1_revoke.sql
@@ -1,9 +1,9 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_credential
--- Release Version 	: 1.2
+-- Release Version 	: 1.2.1
 -- Purpose    		: Revoking Database Alter deployement done for release in mosip_credential DB.       
--- Create By   		: Sadanandegowda DM
--- Created Date		: Sep-2020
+-- Create By   		: Anusha SE
+-- Created Date		: Dec-2023
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
--- db_release_scripts/mosip_idrepo/sql/1.2.1_revoke.sql
+++ db_release_scripts/mosip_idrepo/sql/1.2.1_revoke.sql
@@ -2,8 +2,8 @@
 -- Database Name: mosip_idrepo
 -- Release Version 	: 1.2.1
 -- Purpose    		: Revoking Database Alter deployement done for release in ID Repository DB.       
--- Create By   		: Ram Bhatt
--- Created Date		: Apr-2021
+-- Create By   		: Anusha SE
+-- Created Date		: Dec-2023
 -- 
 -- Modified Date       Modified By       Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
