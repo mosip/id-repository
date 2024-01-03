08146c402a3bb72fc84fb7d068d2a0c49c3984c6
Anusha Sunkada <sunkadaeanusha@gmail.com>
2024-01-03 11:27:16 +0530

    MOSIP-30992 (#867)
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/helper/IdRepoServiceHelper.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/helper/IdRepoServiceHelper.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.PathNotFoundException;
 import io.mosip.idrepository.core.builder.IdentityIssuanceProfileBuilder;
 import io.mosip.idrepository.core.builder.RestRequestBuilder;
 import io.mosip.idrepository.core.constant.IdRepoErrorConstants;
@@ -180,9 +181,13 @@ public class IdRepoServiceHelper {
 
     private List<String> getSupportedHandles(String schema) {
         List<String> supportedHandles = new ArrayList<>();
-        List<String> paths = JsonPath.using(Configuration.builder().options(Option.AS_PATH_LIST).build())
-                .parse(schema)
-                .read("$['properties']['identity']['properties'][*][?(@['handle']==true)]");
+        List<String> paths = new ArrayList<>();
+        try {
+            paths = JsonPath.using(Configuration.builder().options(Option.AS_PATH_LIST).build())
+                    .parse(schema)
+                    .read("$['properties']['identity']['properties'][*][?(@['handle']==true)]");
+        } catch (PathNotFoundException ex) { /*ignore this exception*/ }
+
         paths.forEach( path -> {
             // returns in below format, so need to remove parent paths
             //Eg: "$['properties']['identity']['properties']['phone']"
