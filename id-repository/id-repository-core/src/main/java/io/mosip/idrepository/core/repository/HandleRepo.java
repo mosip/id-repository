b3a043c0c8d4751f123cfbe9732edb2618580112
Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
2024-01-15 16:51:56 +0530

    Fixed review comment
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/repository/HandleRepo.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/repository/HandleRepo.java
@@ -1,10 +1,15 @@
 package io.mosip.idrepository.core.repository;
 
 import io.mosip.idrepository.core.entity.Handle;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@ConditionalOnBean(name = { "idRepoDataSource" })
 public interface HandleRepo extends JpaRepository<Handle, String> {
 
     boolean existsByHandleHash(String handleHash);
