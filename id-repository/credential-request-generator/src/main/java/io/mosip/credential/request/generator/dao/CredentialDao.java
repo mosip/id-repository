153469d9812ff5f99944e21d1fb4eaabae81e4f4
Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
2024-02-16 13:06:25 +0530

    added test methods
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/dao/CredentialDao.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/dao/CredentialDao.java
@@ -1,26 +1,22 @@
 package io.mosip.credential.request.generator.dao;
-import io.mosip.credential.request.generator.entity.CredentialEntity;
-import io.mosip.credential.request.generator.repositary.CredentialRepositary;
-import io.mosip.idrepository.core.logger.IdRepoLogger;
-import io.mosip.idrepository.core.security.IdRepoSecurityManager;
-import io.mosip.kernel.core.logger.spi.Logger;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import org.apache.commons.math3.stat.descriptive.moment.SemiVariance.Direction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+
+import io.mosip.credential.request.generator.entity.CredentialEntity;
+import io.mosip.credential.request.generator.repositary.CredentialRepositary;
+import io.mosip.idrepository.core.logger.IdRepoLogger;
+import io.mosip.idrepository.core.security.IdRepoSecurityManager;
+import io.mosip.kernel.core.logger.spi.Logger;
 
 @Component
 public class CredentialDao {
@@ -40,11 +36,11 @@ public class CredentialDao {
 
     /** The crdential repo. */
     @Autowired
-    private CredentialRepositary<CredentialEntity, String> crdentialRepo;
+    private CredentialRepositary<CredentialEntity, String> credentialRepo;
 
 
     public void update(String batchId, List<CredentialEntity> credentialEntities) {
-        crdentialRepo.saveAll(credentialEntities);
+        credentialRepo.saveAll(credentialEntities);
         LOGGER.info(IdRepoSecurityManager.getUser(), "CredentialDao", "batchid = " + batchId,
                 "Record updated successfully. Total records : " + credentialEntities.size());
     }
@@ -56,7 +52,7 @@ public class CredentialDao {
         Sort sort = Sort.by(Sort.Direction.ASC, "createDateTime");
         Pageable pageable=PageRequest.of(0, pageSize,sort);
         List<CredentialEntity> credentialEntities=new ArrayList<>();
-        Page<CredentialEntity> pagecredentialEntities= crdentialRepo.findCredentialByStatusCode(status, pageable);
+        Page<CredentialEntity> pagecredentialEntities= credentialRepo.findCredentialByStatusCode(status, pageable);
 		if (pagecredentialEntities != null && pagecredentialEntities.getContent() != null && !pagecredentialEntities.getContent().isEmpty()) {
 	      credentialEntities=	pagecredentialEntities.getContent();
 		}
@@ -74,7 +70,7 @@ public class CredentialDao {
         Pageable pageable=PageRequest.of(0, pageSize,sort);
         String[] statusCodes = reprocessStatusCodes.split(",");
         List<CredentialEntity> credentialEntities=new ArrayList<>();
-        Page<CredentialEntity> pagecredentialEntities=  crdentialRepo.findCredentialByStatusCodes(statusCodes,pageable);
+        Page<CredentialEntity> pagecredentialEntities=  credentialRepo.findCredentialByStatusCodes(statusCodes,pageable);
         if (pagecredentialEntities != null && pagecredentialEntities.getContent() != null && !pagecredentialEntities.getContent().isEmpty()) {
   	      credentialEntities=	pagecredentialEntities.getContent();
   		}
@@ -87,32 +83,23 @@ public class CredentialDao {
 
     public Page<CredentialEntity> findByStatusCode(String statusCode, Pageable pageable){
 
-      return crdentialRepo.findByStatusCode(statusCode, pageable);
+      return credentialRepo.findByStatusCode(statusCode, pageable);
     }
     
     public Page<CredentialEntity> findByStatusCodeWithEffectiveDtimes(String statusCode,
 			 LocalDateTime effectiveDTimes,
 			Pageable pageable){
-    	return crdentialRepo.findByStatusCodeWithEffectiveDtimes(statusCode, effectiveDTimes, pageable);
+    	return credentialRepo.findByStatusCodeWithEffectiveDtimes(statusCode, effectiveDTimes, pageable);
     }
 
-
 	public void save(CredentialEntity credential) {
-		crdentialRepo.save(credential);
+		credentialRepo.save(credential);
 		 LOGGER.info(IdRepoSecurityManager.getUser(), "CredentialDao", "requestId = " + credential.getRequestId(),
 	                "Record saved successfully.");
 	}
 
-
-	public void update(CredentialEntity credential) {
-		crdentialRepo.save(credential);
-		 LOGGER.info(IdRepoSecurityManager.getUser(), "CredentialDao", "requestId = " + credential.getRequestId(),
-	                "Record updated successfully.");
-	}
-
-
 	public Optional<CredentialEntity> findById(String requestId) {
-		return crdentialRepo.findById(requestId);
+		return credentialRepo.findById(requestId);
 		
 	}
 }
