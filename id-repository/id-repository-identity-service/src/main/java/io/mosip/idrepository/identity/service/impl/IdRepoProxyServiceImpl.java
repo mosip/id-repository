5bf6d369879c42e4f532a81fd52e29992260ac7c
RitikJain4108 <99730411+RitikJain4108@users.noreply.github.com>
2024-03-15 10:42:53 +0530

    MOSIP-31702-develop:-as-per-the-sonar-report-id-repo-is-having-2-bugs-need-to-fix (#923)
    * fixed nullpointer sonar bug
    
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
    
    * sonar fixes
    
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
    
    * removed interrupted catch block
    
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
    
    ---------
    
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
    Co-authored-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
    Co-authored-by: 61074108 <61074108@5CD12778H4>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoProxyServiceImpl.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoProxyServiceImpl.java
@@ -448,9 +448,9 @@ public class IdRepoProxyServiceImpl implements IdRepoService<IdRequestDTO, IdRes
 		} catch (IdRepoAppUncheckedException e) {
 			mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "extractTemplate", e.getMessage());
 			throw new IdRepoAppException(e.getErrorCode(), e.getErrorText(), e);
-		} catch (InterruptedException e) {
-			mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "extractTemplate", e.getMessage());
-			throw new IdRepoAppException(BIO_EXTRACTION_ERROR, e);
+//		} catch (InterruptedException e) {
+//			mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "extractTemplate", e.getMessage());
+//			throw new IdRepoAppException(BIO_EXTRACTION_ERROR, e);
 		} catch (Exception e) {
 			mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_IMPL, "extractTemplate", e.getMessage());
 			throw new IdRepoAppException(BIO_EXTRACTION_ERROR, e);
