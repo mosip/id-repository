004edbbaf79b8efcf878a8599a43df935430cbc9
Balaji <74903654+balaji-alluru@users.noreply.github.com>
2024-03-24 23:31:58 +0530

    Added caching to credential-request-generator
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>

---
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/SimpleCacheConfig.java
@@ -0,0 +1,47 @@
+package io.mosip.credential.request.generator.api.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.cache.CacheBuilder;
+
+@Configuration
+public class SimpleCacheConfig extends CachingConfigurerSupport {
+	
+	    @Value("${mosip.credential.cache.size:100}")
+	    private Integer cacheMaxSize;
+
+	    @Value("${mosip.credential.cache.expire-in-seconds:60}")
+	    private Integer cacheExpireInSeconds;
+
+
+	    @Bean
+	    @Override
+	    public CacheManager cacheManager() {
+	        SimpleCacheManager cacheManager = new SimpleCacheManager();
+	        cacheManager.setCaches(Arrays.asList(buildMapCache("credential_transaction")));
+	        return cacheManager;
+	    }
+
+	    private ConcurrentMapCache buildMapCache(String name) {
+	        return new ConcurrentMapCache(name,
+	                CacheBuilder.newBuilder()
+	                        .expireAfterWrite(cacheExpireInSeconds, TimeUnit.SECONDS)
+	                        .maximumSize(cacheMaxSize)
+	                        .build()
+	                        .asMap(), true);
+	    }
+	
+}
