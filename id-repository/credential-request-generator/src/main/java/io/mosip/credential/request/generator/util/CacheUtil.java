10e18acf2b05428546235d7438776a8f526ddc45
Balaji <74903654+balaji-alluru@users.noreply.github.com>
2024-04-25 15:14:59 +0530

    Updated Cacheutil
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/util/CacheUtil.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/util/CacheUtil.java
@@ -3,6 +3,7 @@ package io.mosip.credential.request.generator.util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -20,28 +21,24 @@ public class CacheUtil {
 
 	  @Cacheable(value = "credential_transaction", key = "#requestId")
 	  public CredentialIssueStatusResponse setCredentialTransaction(String requestId, CredentialEntity credentialEntity, String id) {
+		  
+		  return createCredentialIssueStatusResponse(requestId, credentialEntity, id);
+	  }
+    
+	  @CachePut(value = "credential_transaction", key = "#requestId")
+	  public CredentialIssueStatusResponse updateCredentialTransaction(String requestId, CredentialEntity credentialEntity, String id) {
+		  
+	      return createCredentialIssueStatusResponse(requestId, credentialEntity, id);
+	  }
+	  
+	  
+	  public CredentialIssueStatusResponse createCredentialIssueStatusResponse(String requestId, CredentialEntity credentialEntity, String id) {
 		  CredentialIssueStatusResponse credentialIssueStatusResponse = new CredentialIssueStatusResponse();
 		  credentialIssueStatusResponse.setId(id);
 		  credentialIssueStatusResponse.setRequestId(requestId);
 		  credentialIssueStatusResponse.setStatusCode(credentialEntity.getStatusCode());
 		  credentialIssueStatusResponse.setUrl(credentialEntity.getDataShareUrl());
 	      return credentialIssueStatusResponse;
-	  }
-    
-	  @CacheEvict(value = "credential_transaction", key = "#requestId")
-	  private void evictCredentialTransaction(String requestId, CredentialEntity credentialEntity) {
-		  // TODO  This method doesn't need todo anything as of now. 
-	      return ;
-	  } 
-	  
-	  public void updateCredentialTransaction(String requestId, CredentialEntity credentialEntity, String id) {
-		  if(getCredentialTransaction(requestId)!=null) {
-			  evictCredentialTransaction(requestId, credentialEntity);
-			  setCredentialTransaction(requestId, credentialEntity, id);
-		  }
-		  else {
-			  setCredentialTransaction(requestId, credentialEntity, id);
-		  }
 	  }
 	  
 	  public CredentialIssueStatusResponse getCredentialTransaction(String requestId) {
