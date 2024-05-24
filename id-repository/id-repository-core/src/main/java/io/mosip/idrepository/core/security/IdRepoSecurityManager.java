1b104bde9ef7977dd299a2aff159fc8402383ec9
Balaji Alluru <74903654+balaji-alluru@users.noreply.github.com>
2024-05-24 13:20:19 +0530

    Revert "[ES-803] (#956)"
    This reverts commit 9be1e30246f5cd5e1afc8426cdecf057909bdc3d.
    
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/security/IdRepoSecurityManager.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/security/IdRepoSecurityManager.java
@@ -27,7 +27,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.mosip.idrepository.core.builder.RestRequestBuilder;
-import io.mosip.idrepository.core.constant.IdRepoConstants;
 import io.mosip.idrepository.core.constant.IdRepoErrorConstants;
 import io.mosip.idrepository.core.constant.RestServicesConstants;
 import io.mosip.idrepository.core.dto.RestRequestDTO;
@@ -313,7 +312,7 @@ public class IdRepoSecurityManager {
 		return getIdHashAndAttributesWithSaltModuloByPlainIdHash(uin, saltRetreivalFunction).get(ID_HASH);
 	}
 	
-	@Cacheable(cacheNames = IdRepoConstants.ID_ATTRIBUTES_CACHE)
+	@Cacheable(cacheNames = "id_attributes")
 	public Map<String, String> getIdHashAndAttributes(String id, IntFunction<String> saltRetreivalFunction) {
 		return getIdHashAndAttributes(id, saltRetreivalFunction, this::getSaltKeyForId);
 	}
@@ -345,7 +344,7 @@ public class IdRepoSecurityManager {
 	
 	@Scheduled(initialDelayString = "${" + IDREPO_CACHE_UPDATE_INTERVAL + ":" + CACHE_UPDATE_DEFAULT_INTERVAL + "}", fixedDelayString = "${" + IDREPO_CACHE_UPDATE_INTERVAL + ":" + CACHE_UPDATE_DEFAULT_INTERVAL + "}")
 	public void evictIdAttributeCacheAtInterval() {
-		Cache idAttrCache = cacheManager.getCache(IdRepoConstants.ID_ATTRIBUTES_CACHE);
+		Cache idAttrCache = cacheManager.getCache("id_attributes");
 		if (Objects.nonNull(idAttrCache))
 			idAttrCache.clear();
 	}
