46ac8d5d3d9549027377481106112d689aff5890
Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
2024-07-15 13:50:20 +0530

    added debug log
    Signed-off-by: Ritik Jain (IN74108) <ritik.61074108@ltimindtree.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/validator/IdRequestValidator.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/validator/IdRequestValidator.java
@@ -242,6 +242,7 @@ public class IdRequestValidator extends BaseIdRepoValidator implements Validator
 							if (method.equals(CREATE)) {
 								idObjectValidator.validateIdObject(idRepoServiceHelper.getSchema(schemaVersion), requestMap, newRegistrationFields);
 							} else {
+								mosipLogger.debug("========================="+requestMap.toString()+"=========================");
 								idObjectValidator.validateIdObject(idRepoServiceHelper.getSchema(schemaVersion), requestMap, updateUinFields);
 							}
 						}
