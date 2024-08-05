a1c215a9311d9f41a4c90f93503a144969e4e658
kameshsr <kameshsr1338@gmail.com>
2024-08-05 16:16:48 +0530

    MOSIP-35065 Fixed Invalid request time error
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/exception/IdRepoExceptionHandler.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/exception/IdRepoExceptionHandler.java
@@ -24,6 +24,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -78,6 +79,21 @@ public class IdRepoExceptionHandler extends ResponseEntityExceptionHandler {
 	/** The id. */
 	@Resource
 	private Map<String, String> id;
+	private final String DateTimeParseExceptionConstant = "DateTimeParseException";
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		Throwable rootCause = getRootCause(ex);
+		mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO, ID_REPO_EXCEPTION_HANDLER,
+				"handleHttpMessageNotReadable - \n" + ExceptionUtils.getStackTrace(Objects.isNull(rootCause) ? ex : rootCause));
+		IdRepoAppException e;
+		if(ex.getMessage().contains(DateTimeParseExceptionConstant)){
+			e = new IdRepoAppException(INVALID_INPUT_PARAMETER.getErrorCode(), String.format(INVALID_INPUT_PARAMETER.getErrorMessage(), REQUEST_TIME));
+			return new ResponseEntity<>(buildExceptionResponse(e, ((ServletWebRequest)request).getHttpMethod(), null), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(buildExceptionResponse(ex, ((ServletWebRequest)request).getHttpMethod(), null), HttpStatus.OK);
+		}
+	}
 
 	/**
 	 * Handles exceptions that are not handled by other methods in
