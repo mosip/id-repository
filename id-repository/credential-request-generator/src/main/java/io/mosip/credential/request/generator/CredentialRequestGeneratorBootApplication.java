f887dc873f8d6b08dd5861f43a6779c423d9b1de
kameshsr <kameshsr1338@gmail.com>
2024-08-09 19:52:40 +0530

    Fixed startup failure
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/CredentialRequestGeneratorBootApplication.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/CredentialRequestGeneratorBootApplication.java
@@ -1,22 +1,19 @@
 package io.mosip.credential.request.generator;
 
+import io.mosip.kernel.websub.api.client.SubscriberClientImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.mosip.credential.request.generator.api.config.CredentialRequestGeneratorConfig;
-import io.mosip.credential.request.generator.entity.CredentialEntity;
 import io.mosip.idrepository.core.config.IdRepoDataSourceConfig;
 import io.mosip.idrepository.core.helper.RestHelper;
 import io.mosip.idrepository.core.security.IdRepoSecurityManager;
 import io.mosip.idrepository.core.util.DummyPartnerCheckUtil;
 import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
-import io.mosip.kernel.dataaccess.hibernate.repository.impl.HibernateRepositoryImpl;
 
 /**
  * The Class CredentialRequestGeneratorApp.
@@ -24,7 +21,7 @@ import io.mosip.kernel.dataaccess.hibernate.repository.impl.HibernateRepositoryI
  */
 @SpringBootApplication
 @Import(value = { java.lang.String.class, DummyPartnerCheckUtil.class, RestHelper.class, IdRepoSecurityManager.class,
-		CredentialRequestGeneratorConfig.class })
+		CredentialRequestGeneratorConfig.class, SubscriberClientImpl.class })
 @ComponentScan(basePackages = { "io.mosip.credential.*","io.mosip.idrepository.*", "io.mosip.kernel.*", "${mosip.auth.adapter.impl.basepackage}" }, excludeFilters = {
 		@ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = { "io.mosip.kernel.dataaccess.hibernate.config.*" }),
 		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = { HibernateDaoConfig.class,
