b6b4c4cbede8c5f969e1e02f740b48312f11d50e
Aravindhan Alagesan <aravindh2090@gmail.com>
2024-10-22 11:40:59 +0530

    Performance related fixes
    Signed-off-by: Aravindhan Alagesan <aravindh2090@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/repositary/CredentialRepositary.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/repositary/CredentialRepositary.java
@@ -14,6 +14,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import io.mosip.credential.request.generator.entity.CredentialEntity;
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 
