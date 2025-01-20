d72ebefb0f2689b214b0d317eeeeba35db41ecab
kameshsr <kameshsr1338@gmail.com>
2025-01-20 19:41:05 +0530

    MOSIP-39028 Migrated salt generator
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/id-repository-salt-generator/src/main/java/io/mosip/idrepository/saltgenerator/SaltGeneratorBootApplication.java
+++ id-repository/id-repository-salt-generator/src/main/java/io/mosip/idrepository/saltgenerator/SaltGeneratorBootApplication.java
@@ -1,9 +1,11 @@
 package io.mosip.idrepository.saltgenerator;
 
+import io.mosip.idrepository.saltgenerator.service.SaltGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.scheduling.ScheduledTasksEndpointAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -12,9 +14,12 @@ import org.springframework.context.ApplicationContext;
  *
  * @author Manoj SP
  */
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={ScheduledTasksEndpointAutoConfiguration.class})  
-public class SaltGeneratorBootApplication {
+//@EnableAutoConfiguration(exclude={ScheduledTasksEndpointAutoConfiguration.class})
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
+public class SaltGeneratorBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private SaltGenerator saltGenerator;
 
 	/**
 	 * The main method.
@@ -25,6 +30,15 @@ public class SaltGeneratorBootApplication {
 		ApplicationContext applicationContext = SpringApplication.run(SaltGeneratorBootApplication.class,
 				args);
 		SpringApplication.exit(applicationContext);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//logger.info(" started......");
+		saltGenerator.start();
+		//logger.info("  Completed......");
+
 	}
 
 }
--- id-repository/id-repository-salt-generator/src/main/java/io/mosip/idrepository/saltgenerator/config/SaltGeneratorIdRepoDataSourceConfig.java
+++ id-repository/id-repository-salt-generator/src/main/java/io/mosip/idrepository/saltgenerator/config/SaltGeneratorIdRepoDataSourceConfig.java
@@ -1,88 +1,80 @@
-package io.mosip.idrepository.saltgenerator.config;
-
-import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_DRIVERCLASSNAME;
-import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_PASSWORD;
-import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_SCHEMA;
-import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_URL;
-import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_USERNAME;
-import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DB_SCHEMA_NAME;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import io.mosip.idrepository.saltgenerator.entity.idrepo.IdentityHashSaltEntity;
-
-/**
- * The Class SaltGeneratorIdMapDataSourceConfig - Provides configuration for Salt
- * generator application.
- *
- * @author Manoj SP
- */
-@Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "io.mosip.idrepository.saltgenerator.repository.idrepo",
-entityManagerFactoryRef = "identityEntityManagerFactory",
-transactionManagerRef= "identityTransactionManager"
-)
-public class SaltGeneratorIdRepoDataSourceConfig {
-	
-	private static final String MOSIP_IDREPO_IDENTITY_DB = "mosip.idrepo.identity.db";
-	
-	@Autowired
-	private Environment env;
-
-	@Bean
-	public DataSource identityDataSource() {
-		String alias = MOSIP_IDREPO_IDENTITY_DB;
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl(env.getProperty(String.format(DATASOURCE_URL.getValue(), alias)));
-		dataSource.setUsername(env.getProperty(String.format(DATASOURCE_USERNAME.getValue(), alias)));
-		dataSource.setPassword(env.getProperty(String.format(DATASOURCE_PASSWORD.getValue(), alias)));
-		dataSource.setSchema(env.getProperty(DB_SCHEMA_NAME.getValue()));
-		dataSource.setDriverClassName(env.getProperty(String.format(DATASOURCE_DRIVERCLASSNAME.getValue(), alias)));
-		return dataSource;
-	}
-
-	   @Bean(name = "identityEntityManagerFactory")
-	   public LocalContainerEntityManagerFactoryBean identityEntityManagerFactory(EntityManagerFactoryBuilder builder) {
-	       return builder
-	               .dataSource(identityDataSource())
-	               .packages(IdentityHashSaltEntity.class)
-	               .properties(additionalProperties())
-	               .build();
-	   }
-	   
-	   /**
-		 * Additional properties.
-		 *
-		 * @return the map
-		 */
-		private Map<String, Object> additionalProperties() {
-			Map<String, Object> jpaProperties = new HashMap<>();
-			jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-			return jpaProperties;
-		}
-
-	   @Bean(name = "identityTransactionManager")
-	   public PlatformTransactionManager identityTransactionManager(
-	           final @Qualifier("identityEntityManagerFactory") LocalContainerEntityManagerFactoryBean memberEntityManagerFactory) {
-	       return new JpaTransactionManager(memberEntityManagerFactory.getObject());
-
-	   }
-	
-}
+//package io.mosip.idrepository.saltgenerator.config;
+//
+//import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_DRIVERCLASSNAME;
+//import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_PASSWORD;
+//import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_SCHEMA;
+//import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_URL;
+//import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DATASOURCE_USERNAME;
+//import static io.mosip.idrepository.saltgenerator.constant.SaltGeneratorConstant.DB_SCHEMA_NAME;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import io.mosip.idrepository.saltgenerator.entity.idrepo.IdentityHashSaltEntity;
+//
+///**
+// * The Class SaltGeneratorIdMapDataSourceConfig - Provides configuration for Salt
+// * generator application.
+// *
+// * @author Manoj SP
+// */
+//@Configuration
+//@EnableTransactionManagement
+//@EnableJpaRepositories(basePackages = "io.mosip.idrepository.saltgenerator.repository.idrepo",
+//entityManagerFactoryRef = "identityEntityManagerFactory",
+//transactionManagerRef= "identityTransactionManager"
+//)
+//public class SaltGeneratorIdRepoDataSourceConfig {
+//
+//	private static final String MOSIP_IDREPO_IDENTITY_DB = "mosip.idrepo.identity.db";
+//
+//	@Autowired
+//	private Environment env;
+//
+//	@Autowired
+//	private DatabaseRouter databaseRouter;
+//
+//
+//	   @Bean(name = "identityEntityManagerFactory")
+//	   public LocalContainerEntityManagerFactoryBean identityEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+//	       return builder
+//	               .dataSource(databaseRouter.identityDataSource())
+//	               .packages(IdentityHashSaltEntity.class)
+//	               .properties(additionalProperties())
+//	               .build();
+//	   }
+//
+//	   /**
+//		 * Additional properties.
+//		 *
+//		 * @return the map
+//		 */
+//		private Map<String, Object> additionalProperties() {
+//			Map<String, Object> jpaProperties = new HashMap<>();
+//			jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//			return jpaProperties;
+//		}
+//
+//	   @Bean(name = "identityTransactionManager")
+//	   public PlatformTransactionManager identityTransactionManager(
+//	           final @Qualifier("identityEntityManagerFactory") LocalContainerEntityManagerFactoryBean memberEntityManagerFactory) {
+//	       return new JpaTransactionManager(memberEntityManagerFactory.getObject());
+//
+//	   }
+//
+//}
