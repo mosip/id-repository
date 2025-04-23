2a9492913775f3033bee30002adf60fbd4888f93
Nidhi0201 <nidhi.k@cyberpwn.com>
2025-04-23 15:22:07 +0530

    [MOSIP-39938-1]: removed logger
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/service/impl/VidServiceImpl.java
+++ id-repository/id-repository-vid-service/src/main/java/io/mosip/idrepository/vid/service/impl/VidServiceImpl.java
@@ -662,10 +662,7 @@ public class VidServiceImpl implements VidService<VidRequestDTO, ResponseWrapper
 	 *             the id repo app exception
 	 */
 	private void checkExpiry(LocalDateTime expiryDTimes) throws IdRepoAppException {
-		System.out.println("expiryDTimes: " + expiryDTimes);
-		System.out.println("currentTime: " + DateUtils.getUTCCurrentDateTime());
 		if (!DateUtils.after(expiryDTimes, DateUtils.getUTCCurrentDateTime())) {
-			System.out.println("inside if block");
 			mosipLogger.error(IdRepoSecurityManager.getUser(), ID_REPO_VID_SERVICE, "checkExpiry",
 					"throwing Expired VID");
 			throw new IdRepoAppException(INVALID_VID.getErrorCode(),
