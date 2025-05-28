a4162b244a357aad1f3b6eaa21a4cd2173970b73
Nidhi0201 <nidhi.k@cyberpwn.com>
2025-05-28 13:00:40 +0530

    [MOSIP-39969]: query fixes
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/dto/RidDTO.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/dto/RidDTO.java
@@ -1,10 +1,12 @@
 package io.mosip.idrepository.core.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class RidDTO {
 
         private String rid;
