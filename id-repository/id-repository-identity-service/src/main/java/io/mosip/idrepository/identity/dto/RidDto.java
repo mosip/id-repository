ceb07f9dbf219202bad2121202648d1711a7634d
Nidhi0201 <nidhi.k@cyberpwn.com>
2025-05-29 15:52:52 +0530

    [MOSIP-39969-1]: updatedDate removed dto
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/dto/RidDto.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/dto/RidDto.java
@@ -1,7 +1,6 @@
 package io.mosip.idrepository.identity.dto;
 
 import lombok.Data;
-import java.time.LocalDateTime;
 
 /**
  * The Get RID dto.
@@ -12,5 +11,4 @@ import java.time.LocalDateTime;
 public class RidDto {
 
 	private String rid;
-	private LocalDateTime updatedDate;
 }
