f2074068de2daf6a6e9833b49f88d2ca84f25061
Md Tarique Azeez <tarique.azeez@technoforte.co.in>
2025-06-05 17:38:26 +0530

    added logger
    Signed-off-by: Md Tarique Azeez <tarique.azeez@technoforte.co.in>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/SkipDecryptionAspect.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/SkipDecryptionAspect.java
@@ -1,7 +1,10 @@
 package io.mosip.credential.request.generator.aspect;
 
 import io.mosip.credential.request.generator.api.annotation.SkipDecryption;
+import io.mosip.credential.request.generator.batch.config.CredentialItemTasklet;
 import io.mosip.credential.request.generator.util.SkipDecryptionContext;
+import io.mosip.idrepository.core.logger.IdRepoLogger;
+import io.mosip.kernel.core.logger.spi.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,8 +14,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class SkipDecryptionAspect {
 
+    private static final Logger LOGGER = IdRepoLogger.getLogger(SkipDecryptionAspect.class);
+
     @Around("@annotation(skipDecryption)")
     public Object aroundSkipDecryption(ProceedingJoinPoint pjp, SkipDecryption skipDecryption) throws Throwable {
+        LOGGER.info(">>>>> AOP activated for SkipDecryption on method: " + pjp.getSignature());
         try {
             SkipDecryptionContext.setSkipDecryption(true);
             return pjp.proceed();
