42850767ba01c4eca2c166018ed98761709c4154
Md Tarique Azeez <tarique.azeez@technoforte.co.in>
2025-06-06 17:19:51 +0530

    using reflextion in aop
    Signed-off-by: Md Tarique Azeez <tarique.azeez@technoforte.co.in>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/SkipDecryptionAspect.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/SkipDecryptionAspect.java
@@ -1,14 +1,16 @@
 package io.mosip.credential.request.generator.aspect;
 
 import io.mosip.credential.request.generator.api.annotation.SkipDecryption;
-import io.mosip.credential.request.generator.batch.config.CredentialItemTasklet;
 import io.mosip.credential.request.generator.util.SkipDecryptionContext;
 import io.mosip.idrepository.core.logger.IdRepoLogger;
 import io.mosip.kernel.core.logger.spi.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.stereotype.Component;
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -16,14 +18,35 @@ public class SkipDecryptionAspect {
 
     private static final Logger LOGGER = IdRepoLogger.getLogger(SkipDecryptionAspect.class);
 
-    @Around("@annotation(skipDecryption)")
-    public Object aroundSkipDecryption(ProceedingJoinPoint pjp,SkipDecryption skipDecryption) throws Throwable {
-        LOGGER.info(">>>>> AOP activated for SkipDecryption on method: " + pjp.getSignature());
-        try {
+    @Around("execution(* io.mosip.credential.request.generator.repositary..*(..))")
+    public Object aroundSkipDecryption(ProceedingJoinPoint pjp) throws Throwable {
+        Method method = resolveMethod(pjp);
+        boolean shouldSkip = method != null && method.isAnnotationPresent(SkipDecryption.class);
+
+        if (shouldSkip) {
+            LOGGER.info(">>>>> AOP activated for SkipDecryption on method: " + method.getName());
             SkipDecryptionContext.setSkipDecryption(true);
+        }
+
+        try {
             return pjp.proceed();
         } finally {
-            SkipDecryptionContext.setSkipDecryption(false);
+            if (shouldSkip) {
+                SkipDecryptionContext.setSkipDecryption(false);
+            }
+        }
+    }
+
+    private Method resolveMethod(ProceedingJoinPoint pjp) {
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method interfaceMethod = signature.getMethod();
+
+        Class<?> targetClass = AopUtils.getTargetClass(pjp.getTarget());
+
+        try {
+            return targetClass.getMethod(interfaceMethod.getName(), interfaceMethod.getParameterTypes());
+        } catch (NoSuchMethodException e) {
+            return interfaceMethod;
         }
     }
 }
