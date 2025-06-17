92d33b7934d27ed200338d5e09c9de31c58e7299
dhanendra06 <60607841+dhanendra06@users.noreply.github.com>
2025-06-17 19:29:23 +0530

    Merge pull request #1266 from kameshsr/MOSIP-41486_release-1.2.3.x
    MOSIP-41872 Created AOP to skip decryption

---
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/CryptoContext.java
@@ -0,0 +1,22 @@
+package io.mosip.credential.request.generator.aspect;
+
+/**
+ * This class is used for passing the context information inside a thread.
+ * We must clear the context information post usage because it can cause issues in thread pool
+ * environment where threads are being shared.
+ *
+ * @author tarique-azeez
+ */
+
+public class CryptoContext {
+
+    private static final ThreadLocal<Boolean> skipDecryption = ThreadLocal.withInitial(() -> false);
+
+    public static void setSkipDecryption(boolean value) {
+        skipDecryption.set(value);
+    }
+
+    public static boolean isSkipDecryption() {
+        return skipDecryption.get();
+    }
+}
