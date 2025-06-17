735f0f9382b899695561248ba23a57ff00296431
kameshsr <kameshsr1338@gmail.com>
2025-06-17 15:08:30 +0530

    MOSIP-41486 corrected comment
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/CryptoContext.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/CryptoContext.java
@@ -1,24 +1,22 @@
 package io.mosip.credential.request.generator.aspect;
 
 /**
- * Holds cryptographic context information (e.g., whether to skip encryption/decryption)
- * using ThreadLocal to ensure it's isolated per request.
- * Commonly used with annotations like @SkipDecryption to control behavior dynamically
- * during AOP or interceptor execution.
- * Always clear the context after use to avoid memory leaks.
+ * This class is used for passing the context information inside a thread.
+ * We must clear the context information post usage because it can cause issues in thread pool
+ * environment where threads are being shared.
  *
  * @author tarique-azeez
  */
 
 public class CryptoContext {
 
-        private static final ThreadLocal<Boolean> skipDecryption = ThreadLocal.withInitial(() -> false);
+    private static final ThreadLocal<Boolean> skipDecryption = ThreadLocal.withInitial(() -> false);
 
-        public static void setSkipDecryption(boolean value) {
-            skipDecryption.set(value);
-        }
+    public static void setSkipDecryption(boolean value) {
+        skipDecryption.set(value);
+    }
 
-        public static boolean isSkipDecryption() {
-            return skipDecryption.get();
-        }
+    public static boolean isSkipDecryption() {
+        return skipDecryption.get();
+    }
 }
