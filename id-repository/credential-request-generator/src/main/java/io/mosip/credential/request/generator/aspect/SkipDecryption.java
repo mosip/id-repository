b86330c8f71b9921f45806685a5aeefcc3862177
kameshsr <kameshsr1338@gmail.com>
2025-06-17 15:11:47 +0530

    MOSIP-41486 corrected comment
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/SkipDecryption.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/aspect/SkipDecryption.java
@@ -6,11 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Custom annotation used to indicate that decryption should be skipped
- * for the annotated method.
- * Typically used in conjunction with AOP to dynamically bypass decryption logic
- * by setting a flag in {@code CryptoContext}.
- * This is useful for internal processes or batch jobs where decrypted data is not required.
+ * This annotation is designed for method. This used to provide an indication to skip the entity
+ * decryption while loading an entity using the JPA. Suggested to be used operations on JPA
+ * entity.
  *
  * @author tarique-azeez
  */
