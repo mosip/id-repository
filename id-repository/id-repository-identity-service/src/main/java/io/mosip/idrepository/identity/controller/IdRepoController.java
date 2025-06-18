94d1fbb66f39a63c8792a01e5287488d5547dec4
Nidhi0201 <nidhi.k@cyberpwn.com>
2025-06-18 17:08:04 +0530

    [MOSIP-39969-2]: method fix
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/controller/IdRepoController.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/controller/IdRepoController.java
@@ -581,8 +581,7 @@ public class IdRepoController {
 			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(hidden = true))),
 			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true))) })
-	public ResponseEntity<ResponseWrapper<IdVidMetaDataResponseDTO>> searchIdVidMetadata(@Validated @RequestBody RequestWrapper<IdVidMetaDataRequestDTO> request,
-																						 @ApiIgnore Errors errors) throws IdRepoAppException {
+	public ResponseEntity<ResponseWrapper<IdVidMetaDataResponseDTO>> searchIdVidMetadata(@Validated @RequestBody RequestWrapper<IdVidMetaDataRequestDTO> request) throws IdRepoAppException {
 
 		IdVidMetaDataRequestDTO metaDataRequest = request.getRequest();
 		String individualId = metaDataRequest.getIndividualId();
