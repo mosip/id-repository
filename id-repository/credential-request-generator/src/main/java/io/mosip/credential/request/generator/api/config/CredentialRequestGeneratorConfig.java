e62ea38dd1a137b28d8f8717a4cc81184eddcdf1
kameshsr <kameshsr1338@gmail.com>
2025-06-20 11:30:23 +0530

    MOSIP-41872 Corrected spacing issue
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/CredentialRequestGeneratorConfig.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/CredentialRequestGeneratorConfig.java
@@ -1,18 +1,9 @@
 package io.mosip.credential.request.generator.api.config;
 
-import io.mosip.credential.request.generator.entity.CredentialEntity;
-import io.mosip.credential.request.generator.interceptor.CredentialTransactionInterceptor;
-import io.mosip.credential.request.generator.util.CryptoUtil;
-import io.mosip.credential.request.generator.util.RestUtil;
-import io.mosip.idrepository.core.builder.RestRequestBuilder;
-import io.mosip.idrepository.core.constant.RestServicesConstants;
-import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
-import io.mosip.kernel.dataaccess.hibernate.repository.impl.HibernateRepositoryImpl;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -25,64 +16,70 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
+import io.mosip.credential.request.generator.entity.CredentialEntity;
+import io.mosip.credential.request.generator.interceptor.CredentialTransactionInterceptor;
+import io.mosip.credential.request.generator.util.RestUtil;
+import io.mosip.idrepository.core.builder.RestRequestBuilder;
+import io.mosip.idrepository.core.constant.RestServicesConstants;
+import io.mosip.kernel.dataaccess.hibernate.config.HibernateDaoConfig;
+import io.mosip.kernel.dataaccess.hibernate.repository.impl.HibernateRepositoryImpl;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 @EnableJpaRepositories(entityManagerFactoryRef = "entityManagerFactory", basePackages = "io.mosip.credential.request.generator.repositary.*", repositoryBaseClass = HibernateRepositoryImpl.class, excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = {"io.mosip.idrepository.core.repository.*"})})
-@EntityScan(basePackageClasses = {CredentialEntity.class})
+		@ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = { "io.mosip.idrepository.core.repository.*" }) })
+@EntityScan(basePackageClasses = { CredentialEntity.class })
 public class CredentialRequestGeneratorConfig extends HibernateDaoConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(CredentialRequestGeneratorConfig.class);
+	private static final Logger logger = LoggerFactory.getLogger(CredentialRequestGeneratorConfig.class);
+	
+	@Autowired
+	@Qualifier("restUtil")
+	private RestUtil restUtil;
+	
+	@Autowired
+	private OpenApiProperties openApiProperties;
+	
+	@Override
+	public Map<String, Object> jpaProperties() {
+		Map<String, Object> jpaProperties = super.jpaProperties();
+		jpaProperties.put("hibernate.session_factory.interceptor", new CredentialTransactionInterceptor(restUtil));
+		return jpaProperties;
+	}
 
-    @Autowired
-    @Qualifier("restUtil")
-    private RestUtil restUtil;
+	@Bean
+	public OpenAPI openApi() {
+		OpenAPI api = new OpenAPI()
+				.components(new Components())
+				.info(new Info()
+						.title(openApiProperties.getInfo().getTitle())
+						.version(openApiProperties.getInfo().getVersion())
+						.description(openApiProperties.getInfo().getDescription())
+						.license(new License()
+								.name(openApiProperties.getInfo().getLicense().getName())
+								.url(openApiProperties.getInfo().getLicense().getUrl())));
 
-    @Autowired
-    private CryptoUtil cryptoUtil;
+		openApiProperties.getService().getServers().forEach(server -> {
+			api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
+		});
+		logger.info("swagger open api bean is ready");
+		return api;
+	}
 
-    @Autowired
-    private OpenApiProperties openApiProperties;
-
-    @Override
-    public Map<String, Object> jpaProperties() {
-        Map<String, Object> jpaProperties = super.jpaProperties();
-        jpaProperties.put("hibernate.session_factory.interceptor", new CredentialTransactionInterceptor(restUtil, cryptoUtil));
-        return jpaProperties;
-    }
-
-    @Bean
-    public OpenAPI openApi() {
-        OpenAPI api = new OpenAPI()
-                .components(new Components())
-                .info(new Info()
-                        .title(openApiProperties.getInfo().getTitle())
-                        .version(openApiProperties.getInfo().getVersion())
-                        .description(openApiProperties.getInfo().getDescription())
-                        .license(new License()
-                                .name(openApiProperties.getInfo().getLicense().getName())
-                                .url(openApiProperties.getInfo().getLicense().getUrl())));
-
-        openApiProperties.getService().getServers().forEach(server -> {
-            api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
-        });
-        logger.info("swagger open api bean is ready");
-        return api;
-    }
-
-    @Bean
-    public GroupedOpenApi groupedOpenApi() {
-        return GroupedOpenApi.builder().group(openApiProperties.getGroup().getName())
-                .pathsToMatch(openApiProperties.getGroup().getPaths().stream().toArray(String[]::new))
-                .build();
-    }
-
-    @Bean
-    public RestRequestBuilder getRestRequestBuilder() {
-        return new RestRequestBuilder(Arrays.stream(RestServicesConstants.values())
-                .map(RestServicesConstants::getServiceName).collect(Collectors.toList()));
-    }
+	@Bean
+	public GroupedOpenApi groupedOpenApi() {
+		return GroupedOpenApi.builder().group(openApiProperties.getGroup().getName())
+				.pathsToMatch(openApiProperties.getGroup().getPaths().stream().toArray(String[]::new))
+				.build();
+	}
+	
+	@Bean
+	public RestRequestBuilder getRestRequestBuilder() {
+		return new RestRequestBuilder(Arrays.stream(RestServicesConstants.values())
+				.map(RestServicesConstants::getServiceName).collect(Collectors.toList()));
+	}
 }
