73fd4c5c426cf9cfeb53dcc5e2bf7881cae25cd7
kameshsr <kameshsr1338@gmail.com>
2025-06-20 13:16:48 +0530

    MOSIP-41872 added performance logic
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/repositary/CredentialRepositary.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/repositary/CredentialRepositary.java
@@ -1,9 +1,11 @@
 package io.mosip.credential.request.generator.repositary;
 
-import io.mosip.credential.request.generator.entity.CredentialEntity;
-import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Lock;
@@ -13,57 +15,57 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import io.mosip.credential.request.generator.entity.CredentialEntity;
+import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
+
 
 
 // TODO: Auto-generated Javadoc
-
 /**
  * The Interface CredentialRepositary.
  *
+ * @author Sowmya
+ * 
+ *         The Interface CredentialRepositary.
  * @param <T> the generic type
  * @param <E> the element type
- * @author Sowmya
- * <p>
- * The Interface CredentialRepositary.
  */
 @Repository
 public interface CredentialRepositary extends BaseRepository<CredentialEntity, String> {
 
-    @Query("SELECT crdn FROM CredentialEntity crdn WHERE crdn.statusCode= :statusCode")
-    Page<CredentialEntity> findByStatusCode(@Param("statusCode") String statusCode, Pageable pageable);
+	@Query("SELECT crdn FROM CredentialEntity crdn WHERE crdn.statusCode= :statusCode")
+	Page<CredentialEntity> findByStatusCode(@Param("statusCode") String statusCode, Pageable pageable);
 
 
-    @Query("SELECT crdn FROM CredentialEntity crdn WHERE crdn.statusCode= :statusCode and crdn.updateDateTime>= :effectiveDTimes")
-    Page<CredentialEntity> findByStatusCodeWithEffectiveDtimes(@Param("statusCode") String statusCode,
-                                                               @Param("effectiveDTimes") LocalDateTime effectiveDTimes,
-                                                               Pageable pageable);
+	@Query("SELECT crdn FROM CredentialEntity crdn WHERE crdn.statusCode= :statusCode and crdn.updateDateTime>= :effectiveDTimes")
+	Page<CredentialEntity> findByStatusCodeWithEffectiveDtimes(@Param("statusCode") String statusCode,
+			@Param("effectiveDTimes") LocalDateTime effectiveDTimes,
+			Pageable pageable);
+	
+	@Transactional
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE) 
+	@QueryHints({ @QueryHint(name = "jakarta.persistence.lock.timeout", value = "1") })
+	@Query("select c from CredentialEntity c where c.statusCode=:statusCode")
+	Page<CredentialEntity> findCredentialByStatusCode(@Param("statusCode")String statusCode, Pageable pageable);
 
-    @Transactional
-    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1")})
-    @Query("select c from CredentialEntity c where c.statusCode=:statusCode")
-    Page<CredentialEntity> findCredentialByStatusCode(@Param("statusCode") String statusCode, Pageable pageable);
 
+	@Transactional
+	@Query(value = "SELECT * FROM credential_transaction ct"
+			+ " WHERE ct.status_code=:statusCode ORDER BY cr_dtimes FOR UPDATE SKIP LOCKED LIMIT :pageSize", nativeQuery = true)
+	List<CredentialEntity> findCredentialByStatusCode(@Param("statusCode")String statusCode, @Param("pageSize") int pageSize);
 
-    @Transactional
-    @Query(value = "SELECT * FROM credential_transaction ct"
-            + " WHERE ct.status_code=:statusCode ORDER BY cr_dtimes FOR UPDATE SKIP LOCKED LIMIT :pageSize", nativeQuery = true)
-    List<CredentialEntity> findCredentialByStatusCode(@Param("statusCode") String statusCode, @Param("pageSize") int pageSize);
-
-    /**
-     * Find credential by status codes.
-     *
-     * @param statusCodes the status codes
-     * @param type        the type
-     * @param pageable    the pageable
-     * @return the page
-     */
-    @Transactional
-    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1")})
-    @Query("SELECT crdn FROM CredentialEntity crdn WHERE crdn.statusCode in :statusCodes ")
-    Page<CredentialEntity> findCredentialByStatusCodes(@Param("statusCodes") String[] statusCodes, Pageable pageable);
+	/**
+	 * Find credential by status codes.
+	 *
+	 * @param statusCodes the status codes
+	 * @param type        the type
+	 * @param pageable    the pageable
+	 * @return the page
+	 */
+	@Transactional
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
+	@QueryHints({ @QueryHint(name = "jakarta.persistence.lock.timeout", value = "1") })
+	@Query("SELECT crdn FROM CredentialEntity crdn WHERE crdn.statusCode in :statusCodes ")
+	Page<CredentialEntity> findCredentialByStatusCodes(@Param("statusCodes") String[] statusCodes,Pageable pageable);
 
 }
