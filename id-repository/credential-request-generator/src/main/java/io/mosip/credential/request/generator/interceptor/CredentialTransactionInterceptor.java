2f604fc6d8d9a1e3cf8a7320d07936f7f70f2762
kameshsr <kameshsr1338@gmail.com>
2025-06-26 09:52:59 +0530

    MOSIP-41872 changed code as per review comment
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/interceptor/CredentialTransactionInterceptor.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/interceptor/CredentialTransactionInterceptor.java
@@ -74,7 +74,7 @@ public class CredentialTransactionInterceptor implements Interceptor {
 
 	private void encryptData(Object entity, Object[] state, String[] propertyNames) {
 		if (entity instanceof CredentialEntity credEntity) {
-			String encryptedData = cryptoUtil.encryptData(CryptoUtil.encodeToURLSafeBase64(credEntity.getRequest().getBytes()));
+            String encryptedData = cryptoUtil.encryptData(CryptoUtil.encodeToURLSafeBase64(credEntity.getRequest().getBytes()));
 			credEntity.setRequest(encryptedData);
 			int indexOfData = Arrays.asList(propertyNames).indexOf(REQUEST);
 			state[indexOfData] = encryptedData;
