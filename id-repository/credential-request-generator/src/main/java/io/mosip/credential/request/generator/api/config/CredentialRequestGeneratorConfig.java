9afa494f323600638aaa96c9066d53dc8f298b52
kameshsr <kameshsr1338@gmail.com>
2025-07-04 17:44:37 +0530

    MOSIP-42088 added deserialization
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/CredentialRequestGeneratorConfig.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/CredentialRequestGeneratorConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -95,6 +96,8 @@ public class CredentialRequestGeneratorConfig extends HibernateDaoConfig {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.registerModule(new JavaTimeModule());
 		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		return mapper;
 	}
+
 }
