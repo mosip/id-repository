9adbf04febf2b11800b4750591fa37175721d44c
kameshsr <kameshsr1338@gmail.com>
2025-07-10 17:01:27 +0530

    MOSIP-42088 Fixed Uin not avaialable in database issue after performance fix
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/CredentialRequestGeneratorConfig.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/api/config/CredentialRequestGeneratorConfig.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -86,10 +85,5 @@ public class CredentialRequestGeneratorConfig extends HibernateDaoConfig {
 	public RestRequestBuilder getRestRequestBuilder() {
 		return new RestRequestBuilder(Arrays.stream(RestServicesConstants.values())
 				.map(RestServicesConstants::getServiceName).collect(Collectors.toList()));
-	}
-
-	@Bean(name = "credentialObjectMapper")
-	public ObjectMapper credentialObjectMapper() {
-		return new ObjectMapper();
 	}
 }
--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/helper/CredentialIssueRequestHelper.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/helper/CredentialIssueRequestHelper.java
@@ -10,6 +10,7 @@ import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.util.CryptoUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +21,7 @@ public class CredentialIssueRequestHelper {
     private io.mosip.credential.request.generator.util.CryptoUtil cryptoUtil;
 
     @Autowired
-    @Qualifier("credentialObjectMapper")
+    @Lazy
     private ObjectMapper objectMapper;
 
     public CredentialServiceRequestDto getCredentialServiceRequestDto(CredentialIssueRequestDto credentialIssueRequestDto, String requestId) throws JsonProcessingException {
