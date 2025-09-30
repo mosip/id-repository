efea3e976b08c16b3b5a9ea2b07868fd1ac3f214
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2025-09-30 14:29:43 +0530

    Corrected file
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/repository/CredentialRequestStatusRepo.java
+++ id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/repository/CredentialRequestStatusRepo.java
@@ -15,10 +15,6 @@ import org.springframework.stereotype.Repository;
 import io.mosip.idrepository.core.entity.CredentialRequestStatus;
 
 import org.springframework.transaction.annotation.Transactional;
-<<<<<<< HEAD
-=======
-
->>>>>>> 808ea4c0 (MOSIP-40982 (#1228))
 /**
  * @author Manoj SP
  *
@@ -59,11 +55,7 @@ public interface CredentialRequestStatusRepo extends JpaRepository<CredentialReq
 	@Transactional
 	@Query(value = "SELECT * FROM credential_request_status crs"
 			+ " WHERE crs.status=:status ORDER BY crs.cr_dtimes asc FOR UPDATE SKIP LOCKED LIMIT :pageSize", nativeQuery = true)
-<<<<<<< HEAD
-	List<CredentialRequestStatus> findByStatus(@Param("status") String status, @Param("pageSize") int pageSize);
-=======
 	List<CredentialRequestStatus> findByStatus(@Param("status") String status,
 											   @Param("pageSize") int pageSize);
 
->>>>>>> 808ea4c0 (MOSIP-40982 (#1228))
 }
