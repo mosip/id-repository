fb8fcc5e8c709e10e6989632f6a03ba0fdc9da7a
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2025-11-14 13:20:20 +0530

    Update credential-batch_job_execution_context.sql
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- db_scripts/mosip_credential/ddl/credential-batch_job_execution_context.sql
+++ db_scripts/mosip_credential/ddl/credential-batch_job_execution_context.sql
@@ -11,7 +11,3 @@ CREATE TABLE credential.batch_job_execution_context
 WITH (
     OIDS = FALSE
 );
-
---PERFORMANCE OPTIMIZATION INDEXES
-CREATE UNIQUE INDEX batch_job_execution_context_pkey ON credential.batch_job_execution_context USING btree (job_execution_id);
-
