aaa960c35eaebb8753591ca4175ada863eddd3c6
Nidhi <nidhi.k@cyberpwn.com>
2025-11-22 18:01:54 +0530

    [MOSIP -43787 ] Added index on credential_transaction for status_code (#1423)
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_scripts/mosip_credential/ddl/credential-credential_transaction.sql
+++ db_scripts/mosip_credential/ddl/credential-credential_transaction.sql
@@ -72,3 +72,6 @@ CREATE INDEX IF NOT EXISTS idx_cred_new_status_cr_dtimes_active ON credential.cr
 CREATE INDEX IF NOT EXISTS idx_cred_status_cr_dtimes_active ON credential.credential_transaction (status_code, cr_dtimes) WHERE is_deleted = false;
 CREATE INDEX IF NOT EXISTS idx_cred_status_upd_dtimes_active ON credential.credential_transaction (status_code, upd_dtimes) WHERE is_deleted = false;
 CREATE INDEX idx_credtran_status_crdtimes ON credential.credential_transaction USING btree (status_code, cr_dtimes);
+
+--index section starts----
+CREATE INDEX IF NOT EXISTS cred_txn_status_code ON credential.credential_transaction USING btree (status_code);
