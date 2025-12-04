482dfb1388ec438bf7e9cfe3ebe88b73dc194f16
Nandhukumar <nandhukumare@gmail.com>
2025-12-04 12:50:56 +0530

    MOSIP-43917 | updated missed index script
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- db_scripts/mosip_idmap/ddl/idmap-vid.sql
+++ db_scripts/mosip_idmap/ddl/idmap-vid.sql
@@ -73,6 +73,4 @@ CREATE INDEX IF NOT EXISTS idx_vid_uinhash_status_expiry ON idmap.vid USING btre
 CREATE INDEX IF NOT EXISTS idx_vid_uinhash_status_expiry_ind ON idmap.vid USING btree (uin_hash, status_code, expiry_dtimes);
 CREATE INDEX IF NOT EXISTS idx_vid_uinhash_status_vidtype_expiry ON idmap.vid USING btree (uin_hash, status_code, vidtyp_code, expiry_dtimes DESC) WHERE (is_deleted = false);
 CREATE INDEX IF NOT EXISTS idx_vid_uinhash_status_vidtype_expiry_ind ON idmap.vid USING btree (uin_hash, status_code, vidtyp_code, expiry_dtimes);
-
-
 -- PERFORMANCE INDEXES END--
