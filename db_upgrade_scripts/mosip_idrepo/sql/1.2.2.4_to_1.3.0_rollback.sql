de1029aaea66beb2b4c09c73c76208d24cc69e0f
Nandhukumar <nandhukumare@gmail.com>
2025-12-12 16:07:13 +0530

    MOSIP-44035 | updated rollback script
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- db_upgrade_scripts/mosip_idrepo/sql/1.2.2.4_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.2.2.4_to_1.3.0_rollback.sql
@@ -1,13 +1,13 @@
 \c mosip_idrepo
 
 --INDEX
-DROP INDEX IF EXISTS idx_uin_uin_hash ON idrepo.uin;
-DROP INDEX IF EXISTS idx_uin_draft_reg_id ON idrepo.uin_draft;
-DROP INDEX IF EXISTS idx_uin_h_reg_id ON idrepo.uin_h;
-DROP INDEX IF EXISTS idx_handle_handle_hash ON idrepo.handle;
-DROP INDEX IF EXISTS idx_handle_uin_hash ON idrepo.handle;
-DROP INDEX IF EXISTS idx_cred_req_NEW_status_cr_dtimes ON idrepo.credential_request_status;
-DROP INDEX IF EXISTS idx_cred_tran_status ON idrepo.credential_request_status;
+DROP INDEX IF EXISTS idrepo.idx_uin_uin_hash;
+DROP INDEX IF EXISTS idrepo.idx_uin_draft_reg_id;
+DROP INDEX IF EXISTS idrepo.idx_uin_h_reg_id;
+DROP INDEX IF EXISTS idrepo.idx_handle_handle_hash;
+DROP INDEX IF EXISTS idrepo.idx_handle_uin_hash;
+DROP INDEX IF EXISTS idrepo.idx_cred_req_NEW_status_cr_dtimes;
+DROP INDEX IF EXISTS idrepo.idx_cred_tran_status;
 
 -- Below script required to rollback from 1.3.0-beta.1 to 1.3.0.
 
@@ -22,7 +22,7 @@ DROP INDEX IF EXISTS idrepo.idx_cred_hash_partner_deleted;
 DROP INDEX IF EXISTS idrepo.idx_cred_expiry_ts;
 DROP INDEX IF EXISTS idrepo.idx_cred_status_cr_dtimes;
 DROP INDEX IF EXISTS idrepo.idx_crs_hash_not_deleted;
-DROP INDEX IF EXISTS idx_uin_status;
+DROP INDEX IF EXISTS idrepo.idx_uin_status;
 
 ALTER TABLE credential_request_status RESET (autovacuum_vacuum_scale_factor, autovacuum_vacuum_threshold, autovacuum_analyze_scale_factor, autovacuum_analyze_threshold);
 ALTER TABLE channel_info RESET (autovacuum_vacuum_scale_factor, autovacuum_vacuum_threshold, autovacuum_analyze_scale_factor, autovacuum_analyze_threshold);
