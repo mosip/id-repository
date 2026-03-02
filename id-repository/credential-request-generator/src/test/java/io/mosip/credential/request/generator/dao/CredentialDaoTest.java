7fa33190cb7518f262c796e8977f64625d874cf1
Nidhi0201 <nidhi.k@cyberpwn.com>
2026-03-02 13:41:08 +0530

    [MOSIP-44511]: defaults reverted from CredentialDao
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- id-repository/credential-request-generator/src/test/java/io/mosip/credential/request/generator/dao/CredentialDaoTest.java
+++ id-repository/credential-request-generator/src/test/java/io/mosip/credential/request/generator/dao/CredentialDaoTest.java
@@ -38,11 +38,6 @@ public class CredentialDaoTest {
     @Mock
     private EncryptedCredentialDao encryptedCredentialDao;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
     public void testUpdate() {
         String batchId = "batch123";
