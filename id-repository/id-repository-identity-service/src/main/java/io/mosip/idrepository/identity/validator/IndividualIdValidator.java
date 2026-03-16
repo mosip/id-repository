e897447390416f0afe81a136e3fed849350fe06a
Nidhi0201 <nidhi.k@cyberpwn.com>
2026-03-16 09:27:00 +0530

    [MOSIP-44544]: Fix validator target issue for IdVidMetadataRequestWrapper in IndividualIdValidator develop
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/validator/IndividualIdValidator.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/validator/IndividualIdValidator.java
@@ -25,7 +25,7 @@ public class IndividualIdValidator extends BaseIdRepoValidator implements Valida
      */
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return IdVidMetadataRequestWrapper.class.isAssignableFrom(clazz);
     }
 
     /**
