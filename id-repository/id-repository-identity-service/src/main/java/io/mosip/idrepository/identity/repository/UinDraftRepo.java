73b64ddca8357f9e1e30bfaee1a20440b03287e5
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-03-30 16:42:27 +0530

    Fixed the draft issue
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/repository/UinDraftRepo.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/repository/UinDraftRepo.java
@@ -56,7 +56,7 @@ public interface UinDraftRepo extends JpaRepository<UinDraft, String> {
 	 * @param regId the registration id
 	 * @return the uin draft
 	 */
-	@EntityGraph(attributePaths = {"biometrics", "documents", "uinData"})
+	@EntityGraph(attributePaths = {"biometrics"})
 	Optional<UinDraft> findByRegId(String regId);
 
 	/**
