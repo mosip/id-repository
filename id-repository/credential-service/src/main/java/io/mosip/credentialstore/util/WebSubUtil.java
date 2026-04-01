c739525dabcfb4c907115646121a427503c67fd3
kameshsr <kameshsr1338@gmail.com>
2026-04-01 14:33:09 +0530

    MOSIP-44780 Added loggers
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/credential-service/src/main/java/io/mosip/credentialstore/util/WebSubUtil.java
+++ id-repository/credential-service/src/main/java/io/mosip/credentialstore/util/WebSubUtil.java
@@ -37,13 +37,40 @@ public class WebSubUtil {
 	@Retryable(value = { WebSubClientException.class,
 			IOException.class }, maxAttemptsExpression = "${mosip.credential.service.retry.maxAttempts}", backoff = @Backoff(delayExpression = "${mosip.credential.service.retry.maxDelay}"))
 	public void publishSuccess(String topic, EventModel eventModel) throws WebSubClientException, IOException {
-		String requestId=eventModel.getEvent().getTransactionId();
-        HttpHeaders httpHeaders=new HttpHeaders();
+
+		// ===== DEBUG PRINTS START =====
+		System.out.println("=== publishSuccess() called ===");
+		System.out.println("topic: " + topic);
+		System.out.println("partnerhuburl: " + partnerhuburl);
+		System.out.println("eventModel: " + eventModel);
+
+		if (eventModel != null) {
+			System.out.println("eventModel.getEvent(): " + eventModel.getEvent());
+
+			if (eventModel.getEvent() != null) {
+				System.out.println("transactionId: " + eventModel.getEvent().getTransactionId());
+				System.out.println("eventId: " + eventModel.getEvent().getId());
+				System.out.println("eventType: " + eventModel.getEvent().getType());
+				System.out.println("eventTimestamp: " + eventModel.getEvent().getTimestamp());
+				System.out.println("eventData: " + eventModel.getEvent().getData());
+			} else {
+				System.out.println("eventModel.getEvent() is NULL !!!");
+			}
+		} else {
+			System.out.println("eventModel is NULL !!!");
+		}
+		// ===== DEBUG PRINTS END =====
+
+		String requestId = eventModel.getEvent().getTransactionId();
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		System.out.println(">>> Calling pb.publishUpdate() now...");
 		pb.publishUpdate(topic, eventModel, MediaType.APPLICATION_JSON_UTF8_VALUE, httpHeaders, partnerhuburl);
+		System.out.println(">>> pb.publishUpdate() SUCCESS");
+
 		LOGGER.info(IdRepoSecurityManager.getUser(), LoggerFileConstant.REQUEST_ID.toString(),
 				requestId,
 				"Publish the update successfully");
-		
 	}
 
 	@Cacheable(value = "topics", key = "{#topic}")
