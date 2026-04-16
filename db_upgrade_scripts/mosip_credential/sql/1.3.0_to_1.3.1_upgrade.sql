999bf7cf8b7c800ad56e377262d50f6898bfaf53
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2026-04-16 11:22:37 +0530

    Added indexes for credential_transaction
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- db_upgrade_scripts/mosip_credential/sql/1.3.0_to_1.3.1_upgrade.sql
+++ db_upgrade_scripts/mosip_credential/sql/1.3.0_to_1.3.1_upgrade.sql
@@ -1,1 +1,7 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+\c mosip_credential
+
+-- PERFORMANCE INDEXES START--
+CREATE INDEX IF NOT EXISTS idx_credential_transaction_status_code ON credential_transaction (status_code);
+CREATE INDEX IF NOT EXISTS idx_credential_transaction_status_upd ON credential_transaction (status_code, upd_dtimes);
+CREATE INDEX IF NOT EXISTS idx_credential_transaction_status_cr ON credential_transaction (status_code, cr_dtimes);
+-- PERFORMANCE INDEXES END--
