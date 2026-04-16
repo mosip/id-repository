5bdad83e35a65d21d115f171c9f6570486d6fe8b
Sradha Mohanty <mohantysradha10@gmail.com>
2026-04-16 17:24:11 +0530

    MOSIP-44709 (#1492)
    Signed-off-by: Sradha Mohanty <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/idrepo/testrunner/MosipTestRunner.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/idrepo/testrunner/MosipTestRunner.java
@@ -83,7 +83,7 @@ public class MosipTestRunner {
 			setLogLevels();
 
 			HealthChecker healthcheck = new HealthChecker();
-			healthcheck.setCurrentRunningModule(BaseTestCase.currentModule);
+			healthcheck.setCurrentRunningModule(GlobalConstants.IDREPO);
 			Thread trigger = new Thread(healthcheck);
 			trigger.start();
 			
