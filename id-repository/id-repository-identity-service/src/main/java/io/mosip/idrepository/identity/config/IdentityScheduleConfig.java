8e209c0e9393a47ffb19e334bc6e2f10343d92a3
kameshsr <kameshsr1338@gmail.com>
2026-04-21 17:00:14 +0530

    MOSIP-44780 increased timing
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/config/IdentityScheduleConfig.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/config/IdentityScheduleConfig.java
@@ -13,7 +13,7 @@ public class IdentityScheduleConfig {
 	@Autowired
 	private CredentialStatusManager credStatusManager;
 	
-	@Scheduled(fixedDelayString = "10000000")
+	@Scheduled(fixedDelayString = "${" + IdRepoConstants.CREDENTIAL_STATUS_JOB_DELAY + ":1000}")
 	public void credentialStatusHandlerJob() {
 		credStatusManager.triggerEventNotifications();
 	}
