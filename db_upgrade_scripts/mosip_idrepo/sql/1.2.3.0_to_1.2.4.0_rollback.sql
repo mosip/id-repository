0827d17e7df47597608ba2a5d16f718a6c26bcdd
nagendra0721 <nagendra0718@gmail.com>
2026-07-06 20:33:33 +0530

    #1582: db script updated
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- db_upgrade_scripts/mosip_idrepo/sql/1.2.3.0_to_1.2.4.0_rollback.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.2.3.0_to_1.2.4.0_rollback.sql
@@ -4,4 +4,6 @@
 
 ALTER TABLE idrepo.handle DROP COLUMN IF EXISTS status;
 
+ALTER TABLE idrepo.credential_request_status ALTER COLUMN individual_id TYPE varchar(500);
+
 ------------------------------------------------------------------------------------------------
--- db_upgrade_scripts/mosip_idrepo/sql/1.2.3.0_to_1.2.4.0_upgrade.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.2.3.0_to_1.2.4.0_upgrade.sql
@@ -3,4 +3,6 @@
 
 ALTER TABLE idrepo.handle ADD COLUMN status character varying(32) NOT NULL DEFAULT 'ACTIVATED';
 
+ALTER TABLE idrepo.credential_request_status ALTER COLUMN individual_id TYPE varchar(1024);
+
 ------------------------------------------------------------------------------------------------
